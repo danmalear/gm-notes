d1308b6488055a64531905113a82f0039007f7ab
Dan Malear <danmalear@gmail.com>
2025-05-21 00:38:54 -0700

    Added campaign template entity and service

--- backend/src/entities/CampaignTemplate.ts
+++ backend/src/entities/CampaignTemplate.ts
@@ -1,10 +1,45 @@
-// import { Column, Entity, PrimaryColumn } from 'typeorm';
+import type { CampaignTemplate as CampaignTemplateDto } from '#dtos/models/CampaignTemplate.ts';
+import { type UUID, randomUUID } from 'crypto';
+import { Entity } from './Entity.ts';
 
-// @Entity()
-// export class CampaignTemplate {
-// 	@PrimaryColumn('uuid')
-// 	Id: string = self.crypto.randomUUID();
+export class CampaignTemplate implements Entity<CampaignTemplateDto> {
+	// Static properties
+	static readonly tableName = 'CampaignTemplate';
+	static readonly idColumn = 'CampaignTemplateId';
 
-// 	@Column('varchar', { length: 63 })
-// 	Name: string = '';
-// }
+	// Static methods
+	/**
+	 *
+	 * @param dto
+	 * @returns
+	 */
+	static fromDto(dto: CampaignTemplateDto) {
+		return new CampaignTemplate({
+			CampaignTemplateId: dto.campaignTemplateId,
+			Name: dto.name,
+		});
+	}
+
+	// Properties
+	CampaignTemplateId: UUID;
+	Name: string | undefined;
+
+	// Constructors
+	constructor(data: Partial<CampaignTemplate>) {
+		this.CampaignTemplateId = data.CampaignTemplateId || randomUUID();
+		this.Name = data.Name;
+	}
+
+	// Methods
+	/**
+	 * Converts this campaign template into its DTO equivalent
+	 * @returns Campaign template DTO
+	 */
+	toDto() {
+		const dto: CampaignTemplateDto = {
+			campaignTemplateId: this.CampaignTemplateId,
+			name: this.Name,
+		};
+		return dto;
+	}
+}
---
+++ backend/src/services/CampaignTemplateService.ts
@@ -0,0 +1,79 @@
+import { type UUID } from 'crypto';
+import { db } from '../db.ts';
+import { CampaignTemplate } from '../entities/CampaignTemplate.ts';
+import { getMessage } from '../helpers/error.ts';
+import { EntityService } from './EntityService.ts';
+
+export class CampaignTemplateService
+	implements EntityService<CampaignTemplate>
+{
+	static readonly tableName = 'CampaignTemplate';
+	static readonly idColumn = 'CampaignTemplateId';
+
+	static _instance: CampaignTemplateService;
+
+	/**
+	 * @returns The singleton instance
+	 */
+	static getInstance() {
+		if (!CampaignTemplateService._instance) {
+			CampaignTemplateService._instance = new CampaignTemplateService();
+		}
+		return CampaignTemplateService._instance;
+	}
+
+	/**
+	 * Retrieves a Campaign Template record from the database by its UUID
+	 * @param id UUID of the Campaign Template to retrieve
+	 * @returns The Campaign Template with the given UUID, or undefined if not found
+	 */
+	async getById(id: UUID): Promise<CampaignTemplate | undefined> {
+		try {
+			return await db<CampaignTemplate>(CampaignTemplate.tableName)
+				.where(CampaignTemplate.idColumn, id)
+				.first();
+		} catch (e) {
+			throw Error(
+				`Error getting ${CampaignTemplate.tableName} by ID: ${getMessage(e)}`,
+			);
+		}
+	}
+
+	/**
+	 * Inserts a new Campaign Template record into the database
+	 * @param data Campaign Template data to insert into database
+	 * @returns The inserted record
+	 */
+	async insert(data: CampaignTemplate) {
+		try {
+			return await db<CampaignTemplate>(CampaignTemplate.tableName)
+				.insert(data)
+				.returning('*')
+				.then((returning) => returning[0]);
+		} catch (e) {
+			throw Error(
+				`Error inserting ${CampaignTemplate.tableName}: ${getMessage(e)}`,
+			);
+		}
+	}
+
+	/**
+	 * Updates an existing Campaign Template database record with new data
+	 * @param id UUId of the existing Campaign Template to update
+	 * @param data New Campaign Template data to update the record with
+	 * @returns The updated record
+	 */
+	async update(id: string, data: CampaignTemplate) {
+		try {
+			return await db<CampaignTemplate>(CampaignTemplate.tableName)
+				.where(CampaignTemplate.idColumn, id)
+				.update(data)
+				.returning('*')
+				.then((returning) => returning[0]);
+		} catch (e) {
+			throw Error(
+				`Error updating ${CampaignTemplate.tableName} with ID ${id}: ${getMessage(e)}`,
+			);
+		}
+	}
+}
