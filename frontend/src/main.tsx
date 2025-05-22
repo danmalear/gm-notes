9c38f504360162a4a608b6672b07e7645c0d3089
Dan Malear <danmalear@gmail.com>
2025-05-21 18:57:54 -0700

    Added Mantine UI core

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,11 +1,16 @@
+import { MantineProvider } from '@mantine/core';
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { RouterProvider } from 'react-router';
-import './index.css';
 import router from './routes.ts';
+
+import '@mantine/core/styles.css';
+import './index.css';
 
 createRoot(document.getElementById('root')!).render(
 	<StrictMode>
-		<RouterProvider router={router} />
+		<MantineProvider defaultColorScheme="dark">
+			<RouterProvider router={router} />
+		</MantineProvider>
 	</StrictMode>,
 );
