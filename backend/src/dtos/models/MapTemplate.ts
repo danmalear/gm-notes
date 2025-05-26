f14a42e58dfd65f21e15b08978a6e8f1bf065609
Dan Malear <danmalear@gmail.com>
2025-05-25 21:06:35 -0700

    Added map template DTOs

---
+++ backend/src/dtos/models/MapTemplate.ts
@@ -0,0 +1,27 @@
+import type { UUID } from 'crypto';
+import type { CampaignTemplateStub } from './CampaignTemplate';
+
+export interface MapTemplateCreate {
+	campaignTemplateId?: UUID;
+	name: string;
+	imagePath: string;
+}
+
+export interface MapTemplateUpdate {
+	id: UUID;
+	name?: string;
+	imagePath?: string;
+}
+
+export interface MapTemplateResponse {
+	id: UUID;
+	name: string;
+	imagePath: string;
+	campaignTemplate?: CampaignTemplateStub;
+}
+
+export interface MapTemplateStub {
+	id: UUID;
+	name: string;
+	imagePath: string;
+}
