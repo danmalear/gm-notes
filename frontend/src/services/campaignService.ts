70eed40c7f88af00d038291b5194aeed0494a1c8
Dan Malear <danmalear@gmail.com>
2025-05-26 00:13:28 -0700

    Added campaign service

---
+++ frontend/src/services/campaignService.ts
@@ -0,0 +1,27 @@
+import type { CampaignCreate, CampaignResponse } from '#dtos/Campaign.ts';
+import type { DataResponse } from '#dtos/DataResponse.ts';
+import type { UUID } from 'crypto';
+import api from './api.ts';
+
+export const getAllCampaigns = async () => {
+	const response = await api.get<DataResponse<CampaignResponse[]>>(`/campaign`);
+
+	return response.data.data;
+};
+
+export const getCampaign = async (id: UUID) => {
+	const response = await api.get<DataResponse<CampaignResponse>>(
+		`/campaign/${id}`,
+	);
+
+	return response.data.data;
+};
+
+export const insertCampaign = async (data: CampaignCreate) => {
+	const response = await api.post<DataResponse<CampaignResponse>>(
+		`/campaign`,
+		data,
+	);
+
+	return response.data.data;
+};
