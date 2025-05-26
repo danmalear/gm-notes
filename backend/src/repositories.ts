566b0da9980be2c54ae1d039ebae83a7eb7f9521
Dan Malear <danmalear@gmail.com>
2025-05-25 23:28:46 -0700

    registered repositories

--- backend/src/repositories.ts
+++ backend/src/repositories.ts
@@ -1,6 +1,10 @@
 import { CampaignRepository } from './repositories/CampaignRepository.ts';
 import { CampaignTemplateRepository } from './repositories/CampaignTemplateRepository.ts';
+import { MapRepository } from './repositories/MapRepository.ts';
+import { MapTemplateRepository } from './repositories/MapTemplateRepository.ts';
 
 export const campaignTemplateRepository = new CampaignTemplateRepository();
-
 export const campaignRepository = new CampaignRepository();
+
+export const mapTemplateRepository = new MapTemplateRepository();
+export const mapRepository = new MapRepository();
