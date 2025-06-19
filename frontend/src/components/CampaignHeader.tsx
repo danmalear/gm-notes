6b7a97934c0a131c190e3f6ffd2f77f8be6a013d
Dan Malear <danmalear@gmail.com>
2025-06-18 17:10:12 -0700

    Formatted CampaignHeader

--- frontend/src/components/CampaignHeader.tsx
+++ frontend/src/components/CampaignHeader.tsx
@@ -1,12 +1,20 @@
 import { CampaignResponse as Campaign } from '#dtos/Campaign.js';
-import { AppShell } from '@mantine/core';
+import { AppShell, Flex } from '@mantine/core';
 
 export interface CampaignHeaderProps extends React.PropsWithChildren {
 	campaign: Campaign;
 }
 
 const CampaignHeader: React.FC<CampaignHeaderProps> = ({ campaign }) => {
-	return <AppShell.Header>{campaign.name}</AppShell.Header>;
+	return (
+		<AppShell.Header>
+			<Flex direction="row" justify="space-between" align="center" h="100%">
+				<div />
+				<h2>{campaign.name}</h2>
+				<div />
+			</Flex>
+		</AppShell.Header>
+	);
 };
 
 export default CampaignHeader;
