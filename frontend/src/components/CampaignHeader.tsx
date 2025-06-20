95133809088fb7958d0d6e94dcd0d136d63dbf1b
Dan Malear <danmalear@gmail.com>
2025-06-19 22:41:36 -0700

    added a back button to campaign header

--- frontend/src/components/CampaignHeader.tsx
+++ frontend/src/components/CampaignHeader.tsx
@@ -1,5 +1,7 @@
 import { CampaignResponse as Campaign } from '#dtos/Campaign.js';
 import { AppShell, Flex } from '@mantine/core';
+import { IconArrowLeft } from '@tabler/icons-react';
+import { NavLink } from 'react-router';
 
 export interface CampaignHeaderProps extends React.PropsWithChildren {
 	campaign: Campaign;
@@ -7,9 +9,19 @@ export interface CampaignHeaderProps extends React.PropsWithChildren {
 
 const CampaignHeader: React.FC<CampaignHeaderProps> = ({ campaign }) => {
 	return (
-		<AppShell.Header>
+		<AppShell.Header px="sm">
 			<Flex direction="row" justify="space-between" align="center" h="100%">
-				<div />
+				<NavLink
+					to="/campaign"
+					end
+					style={{
+						color: 'white',
+						display: 'flex',
+						justifyItems: 'center',
+					}}
+				>
+					<IconArrowLeft size={32} />
+				</NavLink>
 				<h2>{campaign.name}</h2>
 				<div />
 			</Flex>
