a6304727cce0e57d919b32e7f6e09390bdb81d29
Dan Malear <danmalear@gmail.com>
2025-06-19 23:38:33 -0700

    added 404 and error handling middleware to Express

--- backend/server.ts
+++ backend/server.ts
@@ -1,5 +1,11 @@
+import type { MessageResponse } from '#dtos/MessageResponse.ts';
 import cors from 'cors';
-import express from 'express';
+import express, {
+	type NextFunction,
+	type Request,
+	type Response,
+} from 'express';
+import { getMessage } from './src/helpers/error.ts';
 import { routes } from './src/routes/routes.ts';
 
 const app = express();
@@ -13,6 +19,25 @@ routes(app);
 app.get('/', (req, res) => {
 	res.send('Hello World!');
 });
+
+app.use((req: Request, res: Response<MessageResponse>) => {
+	console.error('Unhandled request received');
+	res.status(404).send({ message: `Unknown route: ${req.path}` });
+});
+
+app.use(
+	(
+		err: unknown,
+		_req: Request,
+		res: Response<MessageResponse>,
+		_next: NextFunction,
+	) => {
+		console.error(err instanceof Error ? err.stack : getMessage(err));
+		res
+			.status(500)
+			.send({ message: `Internal server error: ${getMessage(err)}` });
+	},
+);
 
 app.listen(port, () => {
 	console.log(`Server listening on port ${port}`);
