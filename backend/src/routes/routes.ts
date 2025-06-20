ea81425b36e3848c81d2dca88b875931c680ee10
Dan Malear <danmalear@gmail.com>
2025-06-19 23:39:28 -0700

    Added map routes to API

--- backend/src/routes/routes.ts
+++ backend/src/routes/routes.ts
@@ -2,9 +2,13 @@ import type { Express } from 'express';
 import { campaignRoutes } from './campaign.ts';
 import { campaignTemplateRoutes } from './campaignTemplate.ts';
 import { fileRoutes } from './file.ts';
+import { mapRoutes } from './map.ts';
+import { mapTemplateRoutes } from './mapTemplate.ts';
 
 export function routes(app: Express) {
 	campaignTemplateRoutes(app);
 	campaignRoutes(app);
 	fileRoutes(app);
+	mapTemplateRoutes(app);
+	mapRoutes(app);
 }
