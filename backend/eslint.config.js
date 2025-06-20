9507c275887cb38c71d9437dcaa5a9b16d2b9125
Dan Malear <danmalear@gmail.com>
2025-06-19 23:37:00 -0700

    eslint ignore underscored args

--- backend/eslint.config.js
+++ backend/eslint.config.js
@@ -16,5 +16,13 @@ export default tseslint.config(
 			ecmaVersion: 2020,
 			globals: globals.browser,
 		},
+		rules: {
+			'@typescript-eslint/no-unused-vars': [
+				'error',
+				{
+					argsIgnorePattern: '^_',
+				},
+			],
+		},
 	},
 );
