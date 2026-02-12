8d36f5dcb40b67b1ce6cdea947874fb6ddaed0a7
Dan Malear <danmalear@gmail.com>
2026-02-11 19:24:33 -0800

    Added region repository

---
+++ backend/src/repositories/RegionRepository.ts
@@ -0,0 +1,26 @@
+import type { UUID } from 'crypto';
+import { db } from '../db.ts';
+import { type Region, pkColumn, tableName } from '../entities/Region.ts';
+import { getMessage } from '../helpers/error.ts';
+import { Repository } from './Repository.ts';
+
+export class RegionRepository extends Repository<Region> {
+	constructor() {
+		super(tableName, pkColumn);
+	}
+
+	/**
+	 * Retrieves region records from the database for a given map ID
+	 * @param id UUID of the map to get regions for
+	 * @returns The list of regions (empty array if none found)
+	 */
+	async getByMapId(mapId: UUID) {
+		try {
+			return await db<Region>(this.tableName).where('MapId', mapId);
+		} catch (e) {
+			throw Error(
+				`Error getting ${this.tableName} records for map ID ${mapId}: ${getMessage(e)}`,
+			);
+		}
+	}
+}
