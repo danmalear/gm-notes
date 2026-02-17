70304523fd849a7d7dbb9fcae6255ad2b5966e23
Dan Malear <danmalear@gmail.com>
2026-02-16 16:26:01 -0800

    Added extension to import

--- backend/src/entities/Region.ts
+++ backend/src/entities/Region.ts
@@ -1,5 +1,5 @@
 import type { UUID } from 'crypto';
-import { RegionShape } from './RegionShape';
+import { RegionShape } from './RegionShape.ts';
 
 export const tableName = 'Region';
 export const pkColumn = 'RegionId';
