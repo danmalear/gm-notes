4271a695093351c132183c7612ec366ac060056c
Dan Malear <danmalear@gmail.com>
2026-02-16 16:25:05 -0800

    Included shapes with region gets in repository

--- backend/src/repositories/RegionRepository.ts
+++ backend/src/repositories/RegionRepository.ts
@@ -1,22 +1,67 @@
 import type { UUID } from 'crypto';
 import { db } from '../db.ts';
-import { type Region, pkColumn, tableName } from '../entities/Region.ts';
+import {
+	type Region,
+	RegionWithShapes,
+	pkColumn,
+	tableName,
+} from '../entities/Region.ts';
 import { getMessage } from '../helpers/error.ts';
+import { RegionShapeRepository } from './RegionShapeRepository.ts';
 import { Repository } from './Repository.ts';
 
 export class RegionRepository extends Repository<Region> {
+	regionShapeRepository: RegionShapeRepository;
+
 	constructor() {
 		super(tableName, pkColumn);
+		this.regionShapeRepository = new RegionShapeRepository();
+	}
+
+	async addShapes(region: Region) {
+		const shapes = await this.regionShapeRepository.getByRegionId(
+			region.RegionId,
+		);
+		const regionWithShapes: RegionWithShapes = {
+			...region,
+			RegionShapes: shapes,
+		};
+
+		return regionWithShapes;
+	}
+
+	async getById(id: UUID) {
+		const region = await super.getById(id);
+		if (!region) return undefined;
+
+		return await this.addShapes(region);
+	}
+
+	async getAll() {
+		const regions = await super.getAll();
+
+		const regionsWithShapes: RegionWithShapes[] = [];
+		for (const region of regions) {
+			regionsWithShapes.push(await this.addShapes(region));
+		}
+
+		return regionsWithShapes;
 	}
 
 	/**
 	 * Retrieves region records from the database for a given map ID
-	 * @param id UUID of the map to get regions for
+	 * @param mapId UUID of the map to get regions for
 	 * @returns The list of regions (empty array if none found)
 	 */
 	async getByMapId(mapId: UUID) {
 		try {
-			return await db<Region>(this.tableName).where('MapId', mapId);
+			const regions = await db<Region>(this.tableName).where('MapId', mapId);
+
+			const regionsWithShapes: RegionWithShapes[] = [];
+			for (const region of regions) {
+				regionsWithShapes.push(await this.addShapes(region));
+			}
+			return regionsWithShapes;
 		} catch (e) {
 			throw Error(
 				`Error getting ${this.tableName} records for map ID ${mapId}: ${getMessage(e)}`,
