65f49446143bb6f647a7af0533ab6d433f01fa31
Dan Malear <danmalear@gmail.com>
2026-02-17 17:03:16 -0800

    Removed unnecessary relative lightings They would just always be "Darkness" or "Bright Light"

--- backend/src/entities/data-types.ts
+++ backend/src/entities/data-types.ts
@@ -1,9 +1,3 @@
 export type Lighting = 'Darkness' | 'Dim Light' | 'Bright Light';
 // @TODO this will be used in regions to change based on map lighting
-export type RelativeLighting =
-	| Lighting
-	| 'Default'
-	| 'Default+'
-	| 'Default++'
-	| 'Default-'
-	| 'Default--';
+export type RelativeLighting = Lighting | 'Default' | 'Default+' | 'Default-';
