0f71634ba1acc755a66118060d50bc1f36a4d252
Dan Malear <danmalear@gmail.com>
2026-02-17 16:21:58 -0800

    fixed an import

--- frontend/src/contexts/CampaignContext.ts
+++ frontend/src/contexts/CampaignContext.ts
@@ -1,4 +1,4 @@
-import { CampaignResponse } from '#dtos/Campaign.js';
+import type { CampaignResponse } from '#dtos/Campaign.ts';
 import { createContext } from 'react';
 
 export const CampaignContext = createContext<CampaignResponse>(null!);
