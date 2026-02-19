ee8fc1678dbb6127e02b775469780ecae9ced27d
Dan Malear <danmalear@gmail.com>
2026-02-18 20:30:26 -0800

    Added narration service functions

---
+++ frontend/src/services/narrationService.ts
@@ -0,0 +1,14 @@
+import type { DataResponse } from '#dtos/DataResponse.ts';
+import type { NarrationResponse } from '#dtos/Narration.ts';
+import type { UUID } from 'crypto';
+import api from './api.ts';
+
+export const getNarrationsByRegionId = async (regionId: UUID) => {
+	return await api.get<DataResponse<NarrationResponse[]>>(
+		`/narrations?regionId=${regionId}`,
+	);
+};
+
+export const getNarration = async (id: UUID) => {
+	return await api.get<DataResponse<NarrationResponse>>(`/narrations/${id}`);
+};
