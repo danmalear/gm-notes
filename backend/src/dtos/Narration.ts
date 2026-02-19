574916771fc057a7ae3913d8f4c931abd6695442
Dan Malear <danmalear@gmail.com>
2026-02-18 20:13:07 -0800

    Added Narration DTO

---
+++ backend/src/dtos/Narration.ts
@@ -0,0 +1,21 @@
+import type { UUID } from 'crypto';
+
+export interface NarrationQueryParams {
+	regionId?: UUID;
+}
+
+export interface NarrationResponse {
+	id: UUID;
+	// @TODO
+	narrationTemplate?: undefined;
+	name: string;
+	description: string;
+	isRead: boolean;
+}
+
+export interface NarrationStub {
+	id: UUID;
+	descriptionTemplateId?: UUID;
+	name: string;
+	isRead: boolean;
+}
