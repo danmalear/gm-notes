42943c458ba36ad2263338f0f42cbd4e47f9b29c
Dan Malear <danmalear@gmail.com>
2026-02-18 20:20:39 -0800

    Registered narration repository

--- backend/src/repositories/repositories.ts
+++ backend/src/repositories/repositories.ts
@@ -2,6 +2,7 @@ import { CampaignRepository } from './CampaignRepository.ts';
 import { CampaignTemplateRepository } from './CampaignTemplateRepository.ts';
 import { MapRepository } from './MapRepository.ts';
 import { MapTemplateRepository } from './MapTemplateRepository.ts';
+import { NarrationRepository } from './NarrationRepository.ts';
 import { RegionRepository } from './RegionRepository.ts';
 
 export const campaignTemplateRepository = new CampaignTemplateRepository();
@@ -11,3 +12,5 @@ export const mapTemplateRepository = new MapTemplateRepository();
 export const mapRepository = new MapRepository();
 
 export const regionRepository = new RegionRepository();
+
+export const narrationRepository = new NarrationRepository();
