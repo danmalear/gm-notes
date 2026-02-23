d713f504ae3ef3361a992610cfb4270788f04f73
Dan Malear <danmalear@gmail.com>
2026-02-22 16:35:48 -0800

    Added Skill type to entities and DTOs

--- backend/src/entities/data-types.ts
+++ backend/src/entities/data-types.ts
@@ -3,3 +3,23 @@ export type Lighting = 'Darkness' | 'Dim Light' | 'Bright Light';
 export type RelativeLighting = Lighting | 'Default' | 'Default+' | 'Default-';
 
 export type CurrencyUnit = 'CP' | 'SP' | 'EP' | 'GP' | 'PP';
+
+export type Skill =
+	| 'Acrobatics'
+	| 'Animal Handling'
+	| 'Arcana'
+	| 'Athletics'
+	| 'Deception'
+	| 'History'
+	| 'Insight'
+	| 'Intimidation'
+	| 'Investigation'
+	| 'Medicine'
+	| 'Nature'
+	| 'Perception'
+	| 'Performance'
+	| 'Persuasion'
+	| 'Religion'
+	| 'Sleight of Hand'
+	| 'Stealth'
+	| 'Survival';
