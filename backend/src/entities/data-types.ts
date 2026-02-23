e16d2fa677928015e68a0fcb8bd435e408fee8cb
Dan Malear <danmalear@gmail.com>
2026-02-22 16:41:23 -0800

    added action types to entities and DTOs

--- backend/src/entities/data-types.ts
+++ backend/src/entities/data-types.ts
@@ -4,6 +4,20 @@ export type RelativeLighting = Lighting | 'Default' | 'Default+' | 'Default-';
 
 export type CurrencyUnit = 'CP' | 'SP' | 'EP' | 'GP' | 'PP';
 
+export type ActionType =
+	| 'Attack'
+	| 'Dash'
+	| 'Disengage'
+	| 'Dodge'
+	| 'Help'
+	| 'Hide'
+	| 'Influence'
+	| 'Magic'
+	| 'Ready'
+	| 'Search'
+	| 'Study'
+	| 'Utilize';
+
 export type Skill =
 	| 'Acrobatics'
 	| 'Animal Handling'
