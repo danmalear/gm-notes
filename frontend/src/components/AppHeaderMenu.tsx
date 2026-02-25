ddefcca43b65429d17e741270d608f4b9171b685
Dan Malear <danmalear@gmail.com>
2026-02-25 15:42:28 -0800

    Added (broken) implementation of file upload

--- frontend/src/components/AppHeaderMenu.tsx
+++ frontend/src/components/AppHeaderMenu.tsx
@@ -1,17 +1,42 @@
-import { Menu, Modal } from '@mantine/core';
+import { Button, FileInput, Menu, Modal } from '@mantine/core';
 import { IconDotsVertical, IconUpload } from '@tabler/icons-react';
-import { useState } from 'react';
+import React, { type FormEvent, useState } from 'react';
+import { uploadFile } from '../services/fileService';
 
 const AppHeaderMenu: React.FC<React.PropsWithChildren> = ({ children }) => {
-	const [modalOpened, setModalOpened] = useState(false);
+	const [uploadModalOpened, setUploadModalOpened] = useState(false);
+	const [file, setFile] = useState<File | null>(null);
 
 	const handleUploadClicked = () => {
-		setModalOpened(true);
+		setUploadModalOpened(true);
+	};
+
+	const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
+		e.preventDefault();
+		if (file) {
+			const data = new FormData(e.currentTarget);
+			uploadFile(data);
+			console.log(`${file.name} uploaded`);
+		}
 	};
 
 	return (
 		<>
-			<Modal opened={modalOpened} onClose={() => setModalOpened(false)} />
+			<Modal
+				opened={uploadModalOpened}
+				onClose={() => setUploadModalOpened(false)}
+				title="Upload File"
+			>
+				<form onSubmit={handleSubmit} encType="multipart/formdata">
+					<FileInput
+						label="File"
+						placeholder="Upload file"
+						value={file}
+						onChange={setFile}
+					/>
+					<Button type="submit">Upload</Button>
+				</form>
+			</Modal>
 			<Menu>
 				<Menu.Target>
 					<IconDotsVertical cursor="pointer" />
