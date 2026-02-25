3c30d488d113cd85c177aec6e3ee21869d351384
Dan Malear <danmalear@gmail.com>
2026-02-24 17:00:49 -0800

    Menu now opens dummy modal

--- frontend/src/components/AppHeaderMenu.tsx
+++ frontend/src/components/AppHeaderMenu.tsx
@@ -1,28 +1,34 @@
-import { Menu } from '@mantine/core';
+import { Menu, Modal } from '@mantine/core';
 import { IconDotsVertical, IconUpload } from '@tabler/icons-react';
+import { useState } from 'react';
 
 const AppHeaderMenu: React.FC<React.PropsWithChildren> = ({ children }) => {
+	const [modalOpened, setModalOpened] = useState(false);
+
 	const handleUploadClicked = () => {
-		alert('Upload clicked - not implemented');
+		setModalOpened(true);
 	};
 
 	return (
-		<Menu>
-			<Menu.Target>
-				<IconDotsVertical cursor="pointer" />
-			</Menu.Target>
+		<>
+			<Modal opened={modalOpened} onClose={() => setModalOpened(false)} />
+			<Menu>
+				<Menu.Target>
+					<IconDotsVertical cursor="pointer" />
+				</Menu.Target>
 
-			<Menu.Dropdown>
-				<Menu.Label>Development</Menu.Label>
-				<Menu.Item
-					leftSection={<IconUpload size={14} />}
-					onClick={handleUploadClicked}
-				>
-					Upload File
-				</Menu.Item>
-				{children}
-			</Menu.Dropdown>
-		</Menu>
+				<Menu.Dropdown>
+					<Menu.Label>Development</Menu.Label>
+					<Menu.Item
+						leftSection={<IconUpload size={14} />}
+						onClick={handleUploadClicked}
+					>
+						Upload File
+					</Menu.Item>
+					{children}
+				</Menu.Dropdown>
+			</Menu>
+		</>
 	);
 };
 
