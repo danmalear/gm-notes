fa6618fde5eb7b536165fc55283812c520c776d8
Dan Malear <danmalear@gmail.com>
2026-02-27 15:25:29 -0800

    Added enum type to handout entity

--- backend/src/entities/Handout.ts
+++ backend/src/entities/Handout.ts
@@ -3,10 +3,12 @@ import type { UUID } from 'crypto';
 export const tableName = 'Handout';
 export const pkColumn = 'HandoutId';
 
+export type HandoutType = 'Text' | 'Image' | 'File';
+
 export interface Handout {
 	HandoutId: UUID;
 	CampaignId: UUID;
 	Name: string;
-	Type: string;
+	Type: HandoutType;
 	Source: string;
 }
