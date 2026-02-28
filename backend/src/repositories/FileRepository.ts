331074ed7a3b0bc13087ef234648dbf5ea87a492
Dan Malear <danmalear@gmail.com>
2026-02-27 16:17:03 -0800

    resolved TS error

--- backend/src/repositories/FileRepository.ts
+++ backend/src/repositories/FileRepository.ts
@@ -5,4 +5,9 @@ export class FileRepository extends Repository<File> {
 	constructor() {
 		super(tableName, pkColumn);
 	}
+
+	async getById(id: string) {
+		// @ts-expect-error This is a rare case of the ID column not being in UUID format
+		return await super.getById(id);
+	}
 }
