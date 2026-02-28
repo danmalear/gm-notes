48d9de9eaa839d1c40dfb8a605994f24eefe2b72
Dan Malear <danmalear@gmail.com>
2026-02-27 16:32:26 -0800

    Replaced static serving with file get request

--- backend/src/routes/file.ts
+++ backend/src/routes/file.ts
@@ -1,6 +1,6 @@
 import type { MessageResponse } from '#dtos/MessageResponse.ts';
 import type { FileStub } from '#dtos/file.ts';
-import express, { type Express, type Request, type Response } from 'express';
+import { type Express, type Request, type Response } from 'express';
 import multer from 'multer';
 import path from 'path';
 import { fileRepository } from '../repositories.ts';
@@ -10,7 +10,36 @@ const apiNamespace = 'files';
 const upload = multer({ dest: path.resolve('uploads') });
 
 export const fileRoutes = (app: Express) => {
-	app.use(`/${apiNamespace}`, express.static(path.resolve('uploads')));
+	app.get(
+		`/${apiNamespace}/:fileId`,
+		async (req, res: Response<MessageResponse | FileStub>) => {
+			console.log(
+				`File GET request received. params: ${JSON.stringify(req.params)}`,
+			);
+
+			if (req.params.fileId.length !== 32) {
+				res.status(400).send({ message: 'Invalid file ID format' });
+				return;
+			}
+
+			const fileRecord = await fileRepository.getById(req.params.fileId);
+
+			if (!fileRecord) {
+				res.status(404).send({ message: 'File not found' });
+				return;
+			}
+
+			res.download(
+				`${path.resolve('uploads')}/${req.params.fileId}`,
+				fileRecord.FileName,
+				(err) => {
+					if (err) {
+						console.error(err);
+					}
+				},
+			);
+		},
+	);
 
 	app.post(
 		`/${apiNamespace}`,
