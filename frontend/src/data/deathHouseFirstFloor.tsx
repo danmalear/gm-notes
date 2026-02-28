6a945b37d72490343871fb9ac87fd38f6a528602
Dan Malear <danmalear@gmail.com>
2026-02-28 15:47:16 -0800

    Removed todo comment

--- frontend/src/data/deathHouseFirstFloor.tsx
+++ frontend/src/data/deathHouseFirstFloor.tsx
@@ -82,7 +82,6 @@ const firstFloorRegions: {
 				text: 'Durst family portrait',
 				url: 'https://publish-01.obsidian.md/access/7db64b11c71d88572ddc6cd06b888976/images/The%20Durst%20Family.png',
 			},
-			// @TODO DB
 			{
 				text: 'Poem',
 				url: 'https://www.strahdreloaded.com/Act+I+-+Into+the+Mists/Arc+A+-+Escape+From+Death+House#Main+Hall',
