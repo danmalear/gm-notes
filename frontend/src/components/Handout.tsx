89d30485f8fa2652a6caf6274f3061dc70feff73
Dan Malear <danmalear@gmail.com>
2026-02-28 13:28:52 -0800

    Added modal for text handouts

--- frontend/src/components/Handout.tsx
+++ frontend/src/components/Handout.tsx
@@ -11,35 +11,58 @@ export interface HandoutProps {
 
 export const Handout: React.FC<HandoutProps> = ({ handout }) => {
 	const [imageModalOpened, setImageModalOpened] = useState(false);
+	const [textModalOpened, setTextModalOpened] = useState(false);
 
 	const handleOpenImageModalClick = (e: MouseEvent<HTMLAnchorElement>) => {
 		e.preventDefault();
 		setImageModalOpened(true);
 	};
 
+	const handleOpenTextModalClick = (e: MouseEvent<HTMLAnchorElement>) => {
+		e.preventDefault();
+		setTextModalOpened(true);
+	};
+
 	return (
 		<>
 			{'source' in handout ? (
-				<Modal
-					opened={imageModalOpened}
-					onClose={() => setImageModalOpened(false)}
-					title={handout.name}
-				>
-					<Center>
-						<img
-							src={filePath(handout.source)}
-							style={{
-								maxHeight: 500,
-								maxWidth: 500,
-							}}
-						/>
-					</Center>
-				</Modal>
+				<>
+					<Modal
+						opened={imageModalOpened}
+						onClose={() => setImageModalOpened(false)}
+						title={handout.name}
+					>
+						<Center>
+							<img
+								src={filePath(handout.source)}
+								style={{
+									maxHeight: 500,
+									maxWidth: 500,
+								}}
+							/>
+						</Center>
+					</Modal>
+					<Modal
+						opened={textModalOpened}
+						onClose={() => setTextModalOpened(false)}
+						title={handout.name}
+					>
+						<div style={{ width: '100%' }}>
+							{handout.source.split('\n').map((line) => (
+								<Center w="100%" inline>
+									<p>{line.trim()}</p>
+								</Center>
+							))}
+						</div>
+					</Modal>
+				</>
 			) : null}
 			{'name' in handout ? (
 				<li>
 					{handout.type === 'Text' ? (
-						handout.source
+						<a href="#" onClick={handleOpenTextModalClick}>
+							{handout.name}
+						</a>
 					) : handout.type === 'Image' ? (
 						<a href="#" onClick={handleOpenImageModalClick}>
 							{handout.name}
