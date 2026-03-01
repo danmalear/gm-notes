0db2fd3006477fcbe265736efec2d142d4eea86e
Dan Malear <danmalear@gmail.com>
2026-02-28 19:28:56 -0800

    Added image modal to Item component

--- frontend/src/components/Item.tsx
+++ frontend/src/components/Item.tsx
@@ -1,13 +1,16 @@
 import type { LocationItemStub } from '#dtos/item.ts';
+import { MouseEvent, useState } from 'react';
 import type { Item as ItemHC } from '../data/MapData.ts';
 import {
 	getValidHeadingIndex,
 	h,
 	ValidHeadingIndex,
 } from '../helpers/headings.ts';
+import { filePath } from '../services/fileService.ts';
 import Action from './Action.tsx';
 import Collapsible from './Collapsible.tsx';
 import CopyLink from './CopyLink.tsx';
+import ImageModal from './modals/ImageModal.tsx';
 import Trait from './Trait.tsx';
 
 // @TODO accommodate contained items loaded from data
@@ -26,63 +29,88 @@ const Item: React.FC<ItemProps> = ({ item, ...props }) => {
 	const H1 = h[props.topLevelHeading];
 	const H2 = h[getValidHeadingIndex(props.topLevelHeading + 1)];
 
+	const [imageModalOpened, setImageModalOpened] = useState(false);
+
+	const handleOpenImageModalClick = (e: MouseEvent<HTMLAnchorElement>) => {
+		e.preventDefault();
+		setImageModalOpened(true);
+	};
+
 	const hasDetails =
 		item.value ||
 		('detailsLink' in item && item.detailsLink) ||
+		('imageFileId' in item && item.imageFileId) ||
 		item.notes?.length ||
 		('items' in item && item.items?.length) ||
 		('actions' in item && item.actions?.length);
 
 	return hasDetails ? (
-		<Collapsible headingElement={H1} title={headingText(item)}>
-			{item.value ? <Trait label="Value">{item.value}</Trait> : null}
-			{'detailsLink' in item && item.detailsLink ? (
-				<CopyLink href={item.detailsLink}>Details</CopyLink>
+		<>
+			{'imageFileId' in item && item.imageFileId ? (
+				<ImageModal
+					imagePath={filePath(item.imageFileId)}
+					opened={imageModalOpened}
+					onClose={() => setImageModalOpened(false)}
+					title={item.name}
+				/>
 			) : null}
-			{item.notes ? (
-				<ul>
-					{item.notes.map((note, noteIndex) => (
-						<li key={`note-${noteIndex}`}>{note}</li>
-					))}
-				</ul>
-			) : null}
-			{'actions' in item && item.actions.length ? (
-				<Collapsible headingElement={H2} title="Actions">
-					{item.actions.map((action) => (
-						<Action
-							action={action}
-							topLevelHeading={getValidHeadingIndex(props.topLevelHeading + 2)}
-						/>
-					))}
-				</Collapsible>
-			) : null}
-			{'items' in item
-				? item.items?.map((subItem, subIndex) =>
-						subItem.value || subItem.notes?.length ? (
-							<Collapsible
-								key={`item-${subItem.name}-${subIndex}`}
-								headingElement={H2}
-								title={headingText(subItem)}
-							>
-								<div>
-									{subItem.value ? (
-										<Trait label="Value">{subItem.value}</Trait>
-									) : null}
-									{subItem.notes ? (
-										<ul>
-											{subItem.notes.map((note, noteIndex) => (
-												<li key={`note-${noteIndex}`}>{note}</li>
-											))}
-										</ul>
-									) : null}
-								</div>
-							</Collapsible>
-						) : (
-							<H2>{headingText(subItem)}</H2>
-						),
-					)
-				: null}
-		</Collapsible>
+			<Collapsible headingElement={H1} title={headingText(item)}>
+				{item.value ? <Trait label="Value">{item.value}</Trait> : null}
+				{'imageFileId' in item && item.imageFileId ? (
+					<a href="#" onClick={handleOpenImageModalClick}>
+						Image
+					</a>
+				) : null}
+				{'detailsLink' in item && item.detailsLink ? (
+					<CopyLink href={item.detailsLink}>Details</CopyLink>
+				) : null}
+				{item.notes ? (
+					<ul>
+						{item.notes.map((note, noteIndex) => (
+							<li key={`note-${noteIndex}`}>{note}</li>
+						))}
+					</ul>
+				) : null}
+				{'actions' in item && item.actions.length ? (
+					<Collapsible headingElement={H2} title="Actions">
+						{item.actions.map((action) => (
+							<Action
+								action={action}
+								topLevelHeading={getValidHeadingIndex(
+									props.topLevelHeading + 2,
+								)}
+							/>
+						))}
+					</Collapsible>
+				) : null}
+				{'items' in item
+					? item.items?.map((subItem, subIndex) =>
+							subItem.value || subItem.notes?.length ? (
+								<Collapsible
+									key={`item-${subItem.name}-${subIndex}`}
+									headingElement={H2}
+									title={headingText(subItem)}
+								>
+									<div>
+										{subItem.value ? (
+											<Trait label="Value">{subItem.value}</Trait>
+										) : null}
+										{subItem.notes ? (
+											<ul>
+												{subItem.notes.map((note, noteIndex) => (
+													<li key={`note-${noteIndex}`}>{note}</li>
+												))}
+											</ul>
+										) : null}
+									</div>
+								</Collapsible>
+							) : (
+								<H2>{headingText(subItem)}</H2>
+							),
+						)
+					: null}
+			</Collapsible>
+		</>
 	) : (
 		<H1>{headingText(item)}</H1>
 	);
