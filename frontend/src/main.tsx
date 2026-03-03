25080830e3a8f475c9ced7a4a7d34cafdfbf90f1
Dan Malear <danmalear@gmail.com>
2026-03-02 18:15:47 -0800

    Made primary color violet

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,4 +1,4 @@
-import { MantineProvider } from '@mantine/core';
+import { createTheme, MantineProvider } from '@mantine/core';
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { RouterProvider } from 'react-router';
@@ -7,9 +7,13 @@ import router from './routes.tsx';
 import '@mantine/core/styles.css';
 import './index.css';
 
+const theme = createTheme({
+	primaryColor: 'violet',
+});
+
 createRoot(document.getElementById('root')!).render(
 	<StrictMode>
-		<MantineProvider defaultColorScheme="dark">
+		<MantineProvider defaultColorScheme="dark" theme={theme}>
 			<RouterProvider router={router} />
 		</MantineProvider>
 	</StrictMode>,
