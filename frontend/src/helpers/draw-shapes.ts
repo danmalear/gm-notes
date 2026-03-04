2a39dd09f38f5b44cdef08424bb62b5ed0899cad
Dan Malear <danmalear@gmail.com>
2026-03-04 14:57:31 -0800

    Fixed up "makeCoords" functions

--- frontend/src/helpers/draw-shapes.ts
+++ frontend/src/helpers/draw-shapes.ts
@@ -1,46 +1,58 @@
-import type { Coords, Rectangle, Shape } from '#dtos/region.ts';
+import type { Circle, Polygon, Rectangle, Shape } from '#dtos/region.ts';
 
-export const makeCoordsRelative = (img: HTMLImageElement, shape: Shape) => {
-	const isCircle = 'r' in shape;
-	const isRect = 'x1' in shape;
-	const isPoly = 'coords' in shape;
-	if (isCircle) {
+const isCircle = (shape: Shape) => 'r' in shape;
+const isRect = (shape: Shape) => 'x1' in shape;
+const isPoly = (shape: Shape) => 'coords' in shape;
+
+// This is to ensure the makeCoords... functions return the correct types
+type ShapeIdentity<T> = T extends Circle
+	? Circle
+	: T extends Rectangle
+		? Rectangle
+		: T extends Polygon
+			? Polygon
+			: never;
+
+export const makeCoordsRelative = <T extends Shape, R = ShapeIdentity<T>>(
+	img: HTMLImageElement,
+	shape: T,
+): R => {
+	if (isCircle(shape)) {
 		const { x, y, r } = shape;
 
-		const relativeX = (x / img.naturalWidth) * img.width;
-		const relativeY = (y / img.naturalHeight) * img.height;
-		const relativeR = (r / img.naturalWidth) * img.width;
-
-		return `${relativeX},${relativeY},${relativeR}`;
+		return {
+			x: (x / img.naturalWidth) * img.width,
+			y: (y / img.naturalHeight) * img.height,
+			r: (r / img.naturalWidth) * img.width,
+		} as R;
 	}
-	if (isRect) {
+	if (isRect(shape)) {
 		const { x1, y1, x2, y2 } = shape;
 
-		const relativeX1 = (x1 / img.naturalWidth) * img.width;
-		const relativeY1 = (y1 / img.naturalHeight) * img.height;
-		const relativeX2 = (x2 / img.naturalWidth) * img.width;
-		const relativeY2 = (y2 / img.naturalHeight) * img.height;
-		return `${relativeX1},${relativeY1},${relativeX2},${relativeY2}`;
+		return {
+			x1: (x1 / img.naturalWidth) * img.width,
+			y1: (y1 / img.naturalHeight) * img.height,
+			x2: (x2 / img.naturalWidth) * img.width,
+			y2: (y2 / img.naturalHeight) * img.height,
+		} as R;
 	}
-	if (isPoly) {
+	if (isPoly(shape)) {
 		const { coords } = shape;
 
-		const coordsStrings: string[] = [];
-		for (const pair of coords) {
-			const { x, y } = pair;
-			const relativeX = (x / img.naturalWidth) * img.width;
-			const relativeY = (y / img.naturalHeight) * img.height;
-			coordsStrings.push(`${relativeX},${relativeY}`);
-		}
-		return coordsStrings.join(',');
+		return {
+			coords: coords.map((pair) => ({
+				x: (pair.x / img.naturalWidth) * img.width,
+				y: (pair.y / img.naturalHeight) * img.height,
+			})),
+		} as R;
 	}
-	return '';
+	throw Error('Invalid shape specified');
 };
 
-export const makeCoordsStatic = (
+export const makeCoordsStatic = <T extends Shape, R = ShapeIdentity<T>>(
 	img: HTMLImageElement,
-	shape: Shape,
-): Shape => {
+	shape: T,
+): R => {
 	const isCircle = 'r' in shape;
 	const isRect = 'x1' in shape;
 	const isPoly = 'coords' in shape;
@@ -51,7 +63,7 @@ export const makeCoordsStatic = (
 			x: (x / img.width) * img.naturalWidth,
 			y: (y / img.height) * img.naturalHeight,
 			r: (r / img.width) * img.naturalWidth,
-		};
+		} as R;
 	}
 	if (isRect) {
 		const { x1, y1, x2, y2 } = shape;
@@ -61,20 +73,16 @@ export const makeCoordsStatic = (
 			y1: (y1 / img.height) * img.naturalHeight,
 			x2: (x2 / img.width) * img.naturalWidth,
 			y2: (y2 / img.height) * img.naturalHeight,
-		};
+		} as R;
 	}
 	if (isPoly) {
 		const { coords } = shape;
-
-		const coordsStrings: Coords[] = [];
-		for (const pair of coords) {
-			const { x, y } = pair;
-			coordsStrings.push({
-				x: (x / img.width) * img.naturalWidth,
-				y: (y / img.height) * img.naturalHeight,
-			});
-		}
-		return { coords };
+		return {
+			coords: coords.map((pair) => ({
+				x: (pair.x / img.width) * img.naturalWidth,
+				y: (pair.y / img.height) * img.naturalHeight,
+			})),
+		} as R;
 	}
 	throw Error('Invalid shape specified');
 };
