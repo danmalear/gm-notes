50b71110266b53efb620e29ecc0bcfc26fc5478d
Dan Malear <danmalear@gmail.com>
2026-04-02 16:44:53 -0700

    debug console log

--- backend/server.ts
+++ backend/server.ts
@@ -38,4 +38,5 @@ app.use(
 app.listen(port, () => {
 	console.log(`Server listening on port ${port}`);
 	console.log(`NODE_ENV: ${process.env.NODE_ENV}`);
+	console.log(`DATABASE_URL: ${process.env.DATABASE_URL}`);
 });
