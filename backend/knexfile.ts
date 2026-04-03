812d4500e1c02a0669810735ce1b5686b0a8b5a6
Dan Malear <danmalear@gmail.com>
2026-04-02 19:06:13 -0700

    Updated knexfile so dev also uses supabase db

--- backend/knexfile.ts
+++ backend/knexfile.ts
@@ -4,10 +4,8 @@ import type { Knex } from 'knex';
 
 const config: { [key: string]: Knex.Config } = {
 	development: {
-		client: 'sqlite3',
-		connection: {
-			filename: './dev.sqlite3',
-		},
+		client: 'pg',
+		connection: process.env.DATABASE_URL,
 		migrations: {
 			tableName: 'knex_migrations',
 		},
