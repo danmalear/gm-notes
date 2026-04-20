11de409a5cbd95ed6e721a343d1b5fb2ca5d92d8
Dan Malear <danmalear@gmail.com>
2026-04-20 13:32:36 -0700

    refactored server file to use createApp function

--- backend/server.ts
+++ backend/server.ts
@@ -9,32 +9,38 @@ import express, {
 } from 'express';
 import { routes } from './src/routes.ts';
 
-const app = express();
+function createApp() {
+	const app = express();
+
+	app.use(cors());
+	app.use(express.json());
+
+	routes(app);
+
+	app.use((req: Request, res: Response<MessageResponse>) => {
+		console.error('Unhandled request received');
+		res.status(404).send({ message: `Unknown route: ${req.path}` });
+	});
+
+	app.use(
+		(
+			err: unknown,
+			_req: Request,
+			res: Response<MessageResponse>,
+			_next: NextFunction,
+		) => {
+			console.error(err instanceof Error ? err.stack : getMessage(err));
+			res
+				.status(500)
+				.send({ message: `Internal server error: ${getMessage(err)}` });
+		},
+	);
+
+	return app;
+}
+
+const app = createApp();
 const port = process.env.PORT;
-
-app.use(cors());
-app.use(express.json());
-
-routes(app);
-
-app.use((req: Request, res: Response<MessageResponse>) => {
-	console.error('Unhandled request received');
-	res.status(404).send({ message: `Unknown route: ${req.path}` });
-});
-
-app.use(
-	(
-		err: unknown,
-		_req: Request,
-		res: Response<MessageResponse>,
-		_next: NextFunction,
-	) => {
-		console.error(err instanceof Error ? err.stack : getMessage(err));
-		res
-			.status(500)
-			.send({ message: `Internal server error: ${getMessage(err)}` });
-	},
-);
 
 app.listen(port, () => {
 	console.log(`Server listening on port ${port}`);
