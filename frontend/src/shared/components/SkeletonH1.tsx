ad8c2ece397a0692cb27595d45bee410bb80e2cd
Dan Malear <danmalear@gmail.com>
2026-04-22 12:51:51 -0700

    Added skeleton for h1

---
+++ frontend/src/shared/components/SkeletonH1.tsx
@@ -0,0 +1,8 @@
+import { Skeleton } from '@mantine/core';
+import type { FC } from 'react';
+
+const SkeletonH1: FC = () => {
+	return <Skeleton circle height="2.5rem" mb="0.75rem" />;
+};
+
+export default SkeletonH1;
