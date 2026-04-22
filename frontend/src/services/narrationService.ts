1f7c9e8d684efd3468ed821686b515ffe87b3914
Dan Malear <danmalear@gmail.com>
2026-04-22 12:02:56 -0700

    Updated narrationService to use correct types; They're the same, but in case they diverge, it'll be good to have different references

--- frontend/src/services/narrationService.ts
+++ frontend/src/services/narrationService.ts
@@ -1,10 +1,10 @@
 import type { DataResponse } from '#dtos/DataResponse.ts';
-import type { NarrationResponse } from '#dtos/narration.ts';
+import type { NarrationResponse, NarrationStub } from '#dtos/narration.ts';
 import type { UUID } from 'crypto';
 import api from './api.ts';
 
 export const getNarrationsByRegionId = async (regionId: UUID) => {
-	return await api.get<DataResponse<NarrationResponse[]>>(
+	return await api.get<DataResponse<NarrationStub[]>>(
 		`/narrations?regionId=${regionId}`,
 	);
 };
