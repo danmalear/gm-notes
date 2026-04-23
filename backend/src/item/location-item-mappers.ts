05062d0529d9315fc3784ea4bf09698613204c80
Dan Malear <danmalear@gmail.com>
2026-04-23 16:55:47 -0700

    Added location item DTO mappers

---
+++ backend/src/item/location-item-mappers.ts
@@ -0,0 +1,52 @@
+import { toStub as actionToStub } from '#action/action-mappers.ts';
+import type {
+	LocationItemResponse,
+	LocationItemResponseBase,
+	LocationItemStub,
+} from './item-dtos.ts';
+import type { ItemRaw } from './Item.ts';
+import type { LocationItem, LocationItemRaw } from './LocationItem.ts';
+
+export const toDto = (item: LocationItem) => {
+	const locationItemResponseBase: LocationItemResponseBase = {
+		id: item.LocationItemId,
+		locationId: item.LocationId,
+		itemId: item.ItemId,
+		name: item.Name,
+		value:
+			item.Value !== null
+				? `${item.Value} ${item.ValueUnit ?? 'GP'}`
+				: undefined,
+		detailsLink: item.DetailsLink ?? undefined,
+		imageFileId: item.ImageFileId ?? undefined,
+		quantity: item.Quantity,
+		actions: item.Actions.map(actionToStub),
+		notes: item.Notes.map((note) => note.Description),
+	};
+
+	const locationItemResponse: LocationItemResponse = item.IsContainer
+		? {
+				...locationItemResponseBase,
+				isContainer: true,
+				containedItems: item.ContainedItems.map(toStub),
+			}
+		: {
+				...locationItemResponseBase,
+				isContainer: false,
+			};
+
+	return locationItemResponse;
+};
+
+export const toStub = (item: ItemRaw & LocationItemRaw) => {
+	const locationItemStub: LocationItemStub = {
+		id: item.LocationItemId,
+		locationId: item.LocationId,
+		itemId: item.ItemId,
+		name: item.Name,
+		quantity: item.Quantity,
+		isContainer: item.IsContainer,
+	};
+
+	return locationItemStub;
+};
