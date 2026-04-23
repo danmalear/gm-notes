20d02d5ae49c03b21c191f25cac664d49e31274c
Dan Malear <danmalear@gmail.com>
2026-04-23 14:28:59 -0700

    Added aliased DTO paths

--- frontend/src/region/region-dtos.ts
+++ frontend/src/region/region-dtos.ts
@@ -1,10 +1,10 @@
 import type { ActionStub } from '#action/action-dtos.ts';
+import type { RelativeLighting } from '#dtos/data-types.ts';
+import type { LocationItemStub } from '#dtos/item.ts';
 import type { HandoutStub } from '#handout/handout-dtos.ts';
 import type { MapStub } from '#map/map-dtos.ts';
 import type { NarrationStub } from '#narration/narration-dtos.ts';
 import type { UUID } from 'crypto';
-import type { RelativeLighting } from '../dtos/data-types.ts';
-import type { LocationItemStub } from '../dtos/item.ts';
 
 export interface Rectangle {
 	x1: number;
