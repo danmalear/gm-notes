ca53f8ad217fc512b6cd652b58c0cb69529ff40b
Dan Malear <danmalear@gmail.com>
2026-04-23 12:45:16 -0700

    Added getById route for actions

---
+++ backend/src/action/action-routes.ts
@@ -0,0 +1,20 @@
+import type { CommandRouter } from '#command/CommandRouter.ts';
+import { getById } from '#shared/route-utils.ts';
+import type { Express } from 'express';
+import type { ActionRepository } from './ActionRepository.ts';
+import { toDto } from './action-mappers.ts';
+
+export function actionRoutes(
+	app: Express,
+	_commandRouter: CommandRouter,
+	actionRepository: ActionRepository,
+) {
+	const apiNamespace = 'actions';
+
+	getById(app, {
+		apiNamespace,
+		objectDescriptor: 'action',
+		repository: actionRepository,
+		toDto,
+	});
+}
