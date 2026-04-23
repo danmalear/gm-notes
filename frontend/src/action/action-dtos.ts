dee6b3131d63b21c025af68cefcd695a3fb61ea9
Dan Malear <danmalear@gmail.com>
2026-04-23 13:02:51 -0700

    Updated action DTOs to match backend; TS now correctly reporting errors

--- frontend/src/action/action-dtos.ts
+++ frontend/src/action/action-dtos.ts
@@ -2,7 +2,7 @@ import type { AbilityCheckStub } from '#ability-check/ability-check-dtos.ts';
 import type { UUID } from 'crypto';
 import type { ActionType } from '../dtos/data-types.ts';
 
-export interface ActionStub {
+export interface ActionResponse {
 	id: UUID;
 	targetId: UUID;
 	name: string;
@@ -10,4 +10,11 @@ export interface ActionStub {
 	narration?: string;
 	conditions: string[];
 	abilityChecks: AbilityCheckStub[];
+}
+
+export interface ActionStub {
+	id: UUID;
+	targetId: UUID;
+	name: string;
+	type?: ActionType;
 }
