1f24ca04c93049b561bc1d86b18d3a8690f69d3a
Dan Malear <danmalear@gmail.com>
2026-04-23 18:53:46 -0700

    Removed broken reference

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -1,8 +1,6 @@
 import { FileRoutes } from '#file/FileRoutes.ts';
-import { RegionRoutes } from '#region/region-routes.ts';
 import type { Express } from 'express';
 
 export function routes(app: Express) {
 	new FileRoutes().init(app);
-	new RegionRoutes().init(app);
 }
