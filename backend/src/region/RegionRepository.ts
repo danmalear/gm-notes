fe08bc0835991dd038a1fabdfdbb0226af7c0374
Dan Malear <danmalear@gmail.com>
2026-04-23 18:18:26 -0700

    Updated RegionRepository to hydrate members

--- backend/src/region/RegionRepository.ts
+++ backend/src/region/RegionRepository.ts
@@ -1,16 +1,65 @@
+import type { ActionRepository } from '#action/ActionRepository.ts';
+import type { HandoutRepository } from '#handout/HandoutRepository.ts';
+import type { LocationItemRepository } from '#item/LocationItemRepository.ts';
+import type { NarrationRepository } from '#narration/NarrationRepository.ts';
+import type { NoteRepository } from '#note/NoteRepository.ts';
 import { db } from '#shared/db.ts';
 import { getMessage } from '#shared/error.ts';
 import { Repository } from '#shared/Repository.ts';
 import type { UUID } from 'crypto';
-import { pkColumn, tableName, type RegionRaw } from './Region.ts';
+import { pkColumn, tableName, type Region, type RegionRaw } from './Region.ts';
+import type { RegionShapeRepository } from './RegionShapeRepository.ts';
 
-export class RegionRepository extends Repository<RegionRaw, RegionRaw> {
-	constructor() {
+export class RegionRepository extends Repository<RegionRaw, Region> {
+	actionRepository: ActionRepository;
+	// creatureRepository: CreatureRepository;
+	handoutRepository: HandoutRepository;
+	locationItemRepository: LocationItemRepository;
+	narrationRepository: NarrationRepository;
+	noteRepository: NoteRepository;
+	regionShapeRepository: RegionShapeRepository;
+
+	constructor(
+		actionRepository: ActionRepository,
+		// creatureRepository: CreatureRepository,
+		handoutRepository: HandoutRepository,
+		locationItemRepository: LocationItemRepository,
+		narrationRepository: NarrationRepository,
+		noteRepository: NoteRepository,
+		regionShapeRepository: RegionShapeRepository,
+	) {
 		super(tableName, pkColumn);
+		this.actionRepository = actionRepository;
+		// this.creatureRepository = creatureRepository;
+		this.handoutRepository = handoutRepository;
+		this.locationItemRepository = locationItemRepository;
+		this.narrationRepository = narrationRepository;
+		this.noteRepository = noteRepository;
+		this.regionShapeRepository = regionShapeRepository;
 	}
 
-	override async getById(id: UUID): Promise<RegionRaw | undefined> {
-		return await this.getByIdRaw(id);
+	override async getById(id: UUID): Promise<Region | undefined> {
+		const regionRaw = await this.getByIdRaw(id);
+		if (!regionRaw) return undefined;
+
+		const narrations = await this.narrationRepository.getByRegionId(id);
+		// const creatures = await this.creatureRepository.getByLocationId(id);
+		const shapes = await this.regionShapeRepository.getByRegionId(id);
+		const actions = await this.actionRepository.getByTargetId(id);
+		const items = await this.locationItemRepository.getByLocationId(id);
+		const handouts = await this.handoutRepository.getByRegionId(id);
+		const notes = await this.noteRepository.getByEntityId(id);
+
+		return {
+			...regionRaw,
+			Narrations: narrations,
+			// Creatures: creatures,
+			Shapes: shapes,
+			Actions: actions,
+			Items: items,
+			Handouts: handouts,
+			Notes: notes,
+		};
 	}
 
 	/**
