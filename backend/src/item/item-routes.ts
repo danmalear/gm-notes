355bb0b158a333ec57cf9f4669ef452071da39a0
Dan Malear <danmalear@gmail.com>
2026-04-23 17:07:21 -0700

    Added (currently unused) item repo to routes

--- backend/src/item/item-routes.ts
+++ backend/src/item/item-routes.ts
@@ -1,12 +1,14 @@
 import type { CommandRouter } from '#command/CommandRouter.ts';
 import { getById } from '#shared/route-utils.ts';
 import type { Express } from 'express';
+import type { ItemRepository } from './ItemRepository.ts';
 import type { LocationItemRepository } from './LocationItemRepository.ts';
 import { toDto } from './location-item-mappers.ts';
 
 export function itemRoutes(
 	app: Express,
 	_commandRouter: CommandRouter,
+	_itemRepository: ItemRepository,
 	locationItemRepository: LocationItemRepository,
 ) {
 	const apiNamespace = 'items';
