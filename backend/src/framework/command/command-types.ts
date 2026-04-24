9a564992e8034417d15129522622b6da2d5f9eb5
Dan Malear <danmalear@gmail.com>
2026-04-24 15:05:19 -0700

    Removed hanging types

--- backend/src/framework/command/command-types.ts
+++ backend/src/framework/command/command-types.ts
@@ -1,15 +1,3 @@
 import type { UUID } from 'crypto';
-import type { CommandRequest } from './command-dtos.ts';
 
 export type CommandFunction<T extends object> = (command: T) => Promise<UUID>;
-
-export type Commands<DomainRequest extends CommandRequest> = {
-	[C in DomainRequest as C['commandType']]: (
-		command: C['command'],
-	) => Promise<object>;
-};
-
-export interface IDomainCommands<DomainRequest extends CommandRequest> {
-	domain: DomainRequest['domain'];
-	commands: Commands<DomainRequest>;
-}
