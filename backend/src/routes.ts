a18d9bc0ac4fbda33ba62e3af5f07495a5325e3f
Dan Malear <danmalear@gmail.com>
2026-04-23 18:47:39 -0700

    Renamed file to region-response in anticipation of refactor

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -1,5 +1,5 @@
 import { FileRoutes } from '#file/FileRoutes.ts';
-import { RegionRoutes } from '#region/RegionRoutes.ts';
+import { RegionRoutes } from '#region/region-routes.ts';
 import type { Express } from 'express';
 
 export function routes(app: Express) {
