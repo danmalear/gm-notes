11fb319a6eaeba7f070e3c944191825bc5f049bf
Dan Malear <danmalear@gmail.com>
2026-04-26 11:57:18 -0700

    Simplified Command repo typing

--- backend/src/framework/command/CommandRepository.ts
+++ backend/src/framework/command/CommandRepository.ts
@@ -2,7 +2,7 @@ import { Repository } from '#shared/Repository.ts';
 import type { UUID } from 'crypto';
 import { pkColumn, tableName, type Command } from './Command.ts';
 
-export class CommandRepository extends Repository<Command, Command> {
+export class CommandRepository extends Repository<Command> {
 	constructor() {
 		super(tableName, pkColumn);
 	}
