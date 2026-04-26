0196d3b66e3e4ab2acad884298045db239fb8ec7
Dan Malear <danmalear@gmail.com>
2026-04-26 14:28:30 -0700

    Added syntactic sugar to message validation

--- backend/src/framework/command/Command.ts
+++ backend/src/framework/command/Command.ts
@@ -1,7 +1,11 @@
-import type { Message } from '#message/Message.ts';
+import { validateMessage, type Message } from '#message/Message.ts';
 
 export type Command<
 	TContext extends string = string,
 	TRef extends string = string,
 	TData extends object = object,
 > = Message<'Command', TContext, TRef, TData>;
+
+export function validateCommand(obj: unknown): asserts obj is Command {
+	validateMessage(obj, 'Command');
+}
--- backend/src/framework/event/Event.ts
+++ backend/src/framework/event/Event.ts
@@ -1,7 +1,11 @@
-import type { Message } from '#message/Message.ts';
+import { validateMessage, type Message } from '#message/Message.ts';
 
 export type Event<
 	TContext extends string = string,
 	TRef extends string = string,
 	TData extends object = object,
 > = Message<'Event', TContext, TRef, TData>;
+
+export function validateEvent(obj: unknown): asserts obj is Event {
+	validateMessage(obj, 'Event');
+}
