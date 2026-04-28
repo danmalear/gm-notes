68ea03b6310ba28b36431aeb8c733b255ec243f5
Dan Malear <danmalear@gmail.com>
2026-04-28 15:24:42 -0700

    Added shared CommandRequest DTO

--- frontend/src/shared/dtos.ts
+++ frontend/src/shared/dtos.ts
@@ -1,5 +1,16 @@
 import type { UUID } from 'crypto';
 
+export interface CommandRequest<
+	TContext extends string,
+	TRef extends string,
+	TData extends object,
+> {
+	context: TContext;
+	ref: TRef;
+	streamId: UUID | undefined;
+	data: TData;
+}
+
 export interface DataResponse<T> {
 	data: T;
 }
