08ca3c1d9404df05b3260b20f84f8f69eed6c85a
Dan Malear <danmalear@gmail.com>
2026-04-28 15:25:22 -0700

    Fixed campaign DTOs to use new command DTO

--- frontend/src/campaign/campaign-dtos.ts
+++ frontend/src/campaign/campaign-dtos.ts
@@ -1,28 +1,21 @@
 import type { MapStub } from '#map/map-dtos.ts';
+import type { CommandRequest } from '#shared/dtos.ts';
 import type { UUID } from 'crypto';
 
 // Commands
 
-export interface CreateCampaignData {
+export type CampaignCommand<
+	TRef extends string,
+	TData extends object,
+> = CommandRequest<'Campaign', TRef, TData>;
+
+export interface CreateCampaign {
 	name: string;
 }
 
-export interface CreateCampaign {
-	context: 'CampaignCommand';
-	type: 'Create';
-	data: CreateCampaignData;
-}
-
-export interface UpdateCampaignData {
-	id: UUID;
-	name?: string;
-	activeMapId?: UUID;
-}
-
-export interface UpdateCampaign {
-	context: 'CampaignCommand';
-	commandType: 'Update';
-	commandData: UpdateCampaignData;
+export interface CreateCampaignCommand
+	extends CampaignCommand<'Create', CreateCampaign> {
+	streamId: undefined;
 }
 
 // Queries
