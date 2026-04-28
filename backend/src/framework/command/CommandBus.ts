49387b6f68fee2209432cf935bfc5bbf5fc9df75
Dan Malear <danmalear@gmail.com>
2026-04-28 14:46:18 -0700

    Overrode send in CommandBus to validate request

--- backend/src/framework/command/CommandBus.ts
+++ backend/src/framework/command/CommandBus.ts
@@ -1,9 +1,21 @@
 import { MessageBus, type IMessageBus } from '#message/MessageBus.ts';
+import { NotFoundError } from '#shared/error.ts';
+import type { UUID } from 'crypto';
+import type { Command } from './Command.ts';
 
 export type ICommandBus = IMessageBus<'Command'>;
 
 export class CommandBus extends MessageBus<'Command'> implements ICommandBus {
 	constructor() {
 		super('Command');
+	}
+
+	override send<TCommand extends Command>(message: TCommand): Promise<UUID> {
+		if (!this.subscribers[message.context]) {
+			throw new NotFoundError(
+				`Context ${message.context} for command request not found`,
+			);
+		}
+		return super.send(message);
 	}
 }
