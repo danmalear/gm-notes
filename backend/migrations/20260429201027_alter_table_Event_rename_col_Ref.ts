47e9d35d51b1519cadbaeb20ff5f2c77f07c6acc
Dan Malear <danmalear@gmail.com>
2026-04-29 13:11:29 -0700

    Renamed Event type column to Ref

---
+++ backend/migrations/20260429201027_alter_table_Event_rename_col_Ref.ts
@@ -0,0 +1,13 @@
+import type { Knex } from 'knex';
+
+export async function up(knex: Knex): Promise<void> {
+	return knex.schema.alterTable('es_Event', (table) => {
+		table.renameColumn('Type', 'Ref');
+	});
+}
+
+export async function down(knex: Knex): Promise<void> {
+	return knex.schema.alterTable('es_Event', (table) => {
+		table.renameColumn('Ref', 'Type');
+	});
+}
