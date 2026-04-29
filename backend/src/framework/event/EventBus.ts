2f9d3ed090f44097ec85f436a6df679db3ef911f
Dan Malear <danmalear@gmail.com>
2026-04-29 13:01:34 -0700

    Added send override to EventBus

--- backend/src/framework/event/EventBus.ts
+++ backend/src/framework/event/EventBus.ts
@@ -1,9 +1,17 @@
+import type { IMessage } from '#message/IMessage.ts';
 import { MessageBus, type IMessageBus } from '#message/MessageBus.ts';
+import type { UUID } from 'crypto';
 
 export type IEventBus = IMessageBus<'Event'>;
 
 export class EventBus extends MessageBus<'Event'> implements IEventBus {
 	constructor() {
 		super('Event');
+	}
+
+	override send<TEvent extends IMessage<'Event'>>(
+		message: TEvent,
+	): Promise<UUID> {
+		return super.send(message);
 	}
 }
