1394461c4f48bfc49557469fad410c6fd28f7d31
Dan Malear <danmalear@gmail.com>
2026-04-29 22:46:46 -0700

    Fixed websocket URI

--- frontend/src/shared/websocket/websocket.ts
+++ frontend/src/shared/websocket/websocket.ts
@@ -1,6 +1,11 @@
-import { serverBaseUrl } from '#shared/api.ts';
+const baseUrl = import.meta.env.VITE_SERVER_BASE_URL;
+const port = import.meta.env.VITE_WS_PORT;
 
-const ws = new WebSocket(`${serverBaseUrl}`);
+const ws = new WebSocket(`${baseUrl}:${port}`);
+
+ws.addEventListener('open', () => {
+	console.log(`WebSocket connection opened`);
+});
 
 ws.addEventListener('error', (e) => {
 	console.error(`WEBSOCKET ERROR: ${e}`);
