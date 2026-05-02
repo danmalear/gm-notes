121d5c85449c7ccfb0b358a2b8ff10c7329cb5cd
Dan Malear <danmalear@gmail.com>
2026-05-01 21:35:45 -0700

    Added custom hook to use new context

---
+++ frontend/src/shared/event-listeners/useEventListener.ts
@@ -0,0 +1,32 @@
+import { EventListenerContext } from '#shared/event-listeners/EventListenerContext.ts';
+import type { UUID } from 'crypto';
+import { useContext } from 'react';
+
+export interface EventOpts {
+	context: string;
+	ref: string;
+	streamId?: UUID;
+}
+
+export const useEventListener = (
+	key: string,
+	{ context, ref, streamId }: EventOpts,
+	handler: (event: object) => void,
+) => {
+	const allListeners = useContext(EventListenerContext);
+	if (!allListeners[context]) {
+		allListeners[context] = {};
+	}
+	const contextListeners = allListeners[context]!;
+	if (!contextListeners[ref]) {
+		contextListeners[ref] = {
+			all: [],
+		};
+	}
+	const refListeners = contextListeners[ref]!;
+
+	const listenerId = streamId ?? 'all';
+
+	if (refListeners[listenerId].find((listener) => listener.key === key)) return;
+	refListeners[listenerId].push({ key, handle: handler });
+};
