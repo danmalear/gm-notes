839f4c11f2842e7bef1a75f44d0e2884dbfb8117
Dan Malear <danmalear@gmail.com>
2026-05-04 11:24:39 -0700

    Websocket reconnect on close

--- frontend/src/websocket/websocket.ts
+++ frontend/src/websocket/websocket.ts
@@ -1,18 +1,37 @@
+import { getMessage } from '#shared/error.ts';
+
+const maxDepth = 50;
+
+let ws: WebSocket;
+let depth = 0;
+
+function startWebsocket(uri: string) {
+	ws = new WebSocket(uri);
+
+	ws.addEventListener('open', () => {
+		console.log(`WebSocket connection opened at ${uri}`);
+		depth = 0;
+	});
+
+	ws.addEventListener('close', () => {
+		console.log('Websocket connection closed');
+		if (depth < maxDepth) {
+			depth++;
+			console.log(`Attempting to reconnect to server, depth ${depth}`);
+			setTimeout(() => startWebsocket(uri), 2000);
+		}
+	});
+
+	ws.addEventListener('error', (e) => {
+		console.error(`WEBSOCKET ERROR: ${getMessage(e)}`);
+	});
+
+	return ws;
+}
+
 const baseUrl = import.meta.env.VITE_SERVER_BASE_URL;
 const port = import.meta.env.VITE_WS_PORT;
 
-const ws = new WebSocket(`${baseUrl}:${port}`);
-
-ws.addEventListener('open', () => {
-	console.log(`WebSocket connection opened`);
-});
-
-ws.addEventListener('close', () => {
-	console.log('Websocket connection closed');
-});
-
-ws.addEventListener('error', (e) => {
-	console.error(`WEBSOCKET ERROR: ${e}`);
-});
+ws = startWebsocket(`${baseUrl}:${port}`);
 
 export default ws;
