761267fcff292c0e49b7abf430fc1df507fa21e8
Dan Malear <danmalear@gmail.com>
2026-05-05 13:50:45 -0700

    Took out generic type from IEventSubscriber

--- backend/src/framework/event/IEventSubscriber.ts
+++ backend/src/framework/event/IEventSubscriber.ts
@@ -1,7 +1,4 @@
 import type { IMessageSubscriber } from '#message/IMessageSubscriber.ts';
 import type { Event } from './Event.ts';
 
-export type IEventSubscriber<TEvent extends Event> = IMessageSubscriber<
-	'Event',
-	TEvent
->;
+export type IEventSubscriber = IMessageSubscriber<'Event', Event>;
