6c607de6c6fad6e581c59962fcdb9527222fcd85
Dan Malear <danmalear@gmail.com>
2026-05-15 14:51:19 -0700

    Added FakeEventBus class to replace const

---
+++ backend/src/framework/event/fakes/FakeEventBus.ts
@@ -0,0 +1,44 @@
+import type { EventRepository } from '#event/event-repository.ts';
+import type { Event } from '#event/Event.ts';
+import type { IMessageSubscriber } from '#framework/message/IMessageSubscriber.ts';
+import { FakeStreamRepository } from '#framework/stream/fakes/FakeStreamRepository.ts';
+import type { StreamRepository } from '#framework/stream/stream-repository.ts';
+import { fakeWss } from '#framework/test/websocket-fake.ts';
+import type { UUID } from 'crypto';
+import { randomUUID } from 'crypto';
+import type { WebSocketServer } from 'ws';
+import type { EventBus } from '../EventBus.ts';
+import { FakeEventRepository } from './FakeEventRepository.ts';
+
+export class FakeEventBus implements EventBus {
+	eventRepository: EventRepository;
+	streamRepository: StreamRepository;
+	wss: WebSocketServer;
+	subscribers: Record<
+		string,
+		IMessageSubscriber<'Event', Event<string, string, object>>[]
+	>;
+
+	constructor() {
+		this.eventRepository = new FakeEventRepository();
+		this.streamRepository = new FakeStreamRepository();
+		this.wss = fakeWss;
+		this.subscribers = {};
+	}
+
+	clone() {
+		return new FakeEventBus();
+	}
+
+	async send(): Promise<UUID> {
+		return randomUUID();
+	}
+
+	cloneSubscribers() {
+		return {};
+	}
+
+	subscribe() {
+		return;
+	}
+}
