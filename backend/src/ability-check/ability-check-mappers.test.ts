20b893797878b7ec08cf68506541e0a482170bd9
Dan Malear <danmalear@gmail.com>
2026-05-19 10:23:00 -0700

    Moved test data for ability checks into separate fie

--- backend/src/ability-check/ability-check-mappers.test.ts
+++ backend/src/ability-check/ability-check-mappers.test.ts
@@ -1,173 +1,39 @@
-import type { NarrationRec } from '#narration/narration-repository.ts';
 import assert from 'assert';
-import { randomUUID } from 'crypto';
 import { suite, test } from 'node:test';
-import type {
-	AbilityCheckResponse,
-	AbilityCheckStub,
-} from './ability-check-dtos.ts';
 import { toDto, toStub } from './ability-check-mappers.ts';
-import type {
-	AbilityCheckRec,
-	AbilityCheckRefRec,
-} from './ability-check-repository.ts';
+import {
+	abilityCheckDto,
+	abilityCheckDtoMin,
+	abilityCheckRec,
+	abilityCheckRecMin,
+	abilityCheckRefRec,
+	abilityCheckRefRecMin,
+	abilityCheckStub,
+	abilityCheckStubMin,
+} from './fakes/ability-check-data-fake.ts';
 
 suite('Ability Check mappers', () => {
-	const abilityCheckId = randomUUID();
-	const actionId = randomUUID();
-	const skill = 'Acrobatics';
-	const dc = 15;
-	const successId = randomUUID();
-	const failureId = randomUUID();
-	const critSuccessId = randomUUID();
-	const critFailureId = randomUUID();
-
 	suite('toDto', () => {
 		test('correctly maps a complete record to a DTO', () => {
-			const success: NarrationRec = {
-				NarrationId: successId,
-				NarrationTemplateId: null,
-				Name: 'Success Narration',
-				Description: 'Success Narration Description',
-				IsRead: true,
-			};
-			const failure: NarrationRec = {
-				NarrationId: failureId,
-				NarrationTemplateId: null,
-				Name: 'Failure Narration',
-				Description: 'Failure Narration Description',
-				IsRead: false,
-			};
-			const critSuccess: NarrationRec = {
-				NarrationId: critSuccessId,
-				NarrationTemplateId: null,
-				Name: 'Critical Success Narration',
-				Description: 'Critical Success Narration Description',
-				IsRead: true,
-			};
-			const critFailure: NarrationRec = {
-				NarrationId: critFailureId,
-				NarrationTemplateId: null,
-				Name: 'Critical Failure Narration',
-				Description: 'Critical Failure Narration Description',
-				IsRead: false,
-			};
-
-			const abilityCheckRec: AbilityCheckRefRec = {
-				AbilityCheckId: abilityCheckId,
-				ActionId: actionId,
-				Skill: skill,
-				DC: dc,
-				SuccessNarrationId: successId,
-				FailureNarrationId: failureId,
-				CriticalSuccessNarrationId: critSuccessId,
-				CriticalFailureNarrationId: critFailureId,
-				SuccessNarration: success,
-				FailureNarration: failure,
-				CriticalSuccessNarration: critSuccess,
-				CriticalFailureNarration: critFailure,
-			};
-
-			const abilityCheckDto: AbilityCheckResponse = {
-				id: abilityCheckId,
-				actionId,
-				skill,
-				dc,
-				successNarration: 'Success Narration Description',
-				failureNarration: 'Failure Narration Description',
-				criticalSuccessNarration: 'Critical Success Narration Description',
-				criticalFailureNarration: 'Critical Failure Narration Description',
-			};
-
-			const testDto = toDto(abilityCheckRec);
+			const testDto = toDto(abilityCheckRefRec);
 			assert.deepStrictEqual(testDto, abilityCheckDto);
 		});
 
 		test('correctly maps a minimal record to a DTO', () => {
-			const abilityCheckRec: AbilityCheckRefRec = {
-				AbilityCheckId: abilityCheckId,
-				ActionId: actionId,
-				Skill: skill,
-				DC: dc,
-				SuccessNarrationId: null,
-				FailureNarrationId: null,
-				CriticalSuccessNarrationId: null,
-				CriticalFailureNarrationId: null,
-				SuccessNarration: null,
-				FailureNarration: null,
-				CriticalSuccessNarration: null,
-				CriticalFailureNarration: null,
-			};
-
-			const abilityCheckDto: AbilityCheckResponse = {
-				id: abilityCheckId,
-				actionId,
-				skill,
-				dc,
-				successNarration: undefined,
-				failureNarration: undefined,
-				criticalSuccessNarration: undefined,
-				criticalFailureNarration: undefined,
-			};
-
-			const testDto = toDto(abilityCheckRec);
-			assert.deepStrictEqual(testDto, abilityCheckDto);
+			const testDto = toDto(abilityCheckRefRecMin);
+			assert.deepStrictEqual(testDto, abilityCheckDtoMin);
 		});
 	});
 
 	suite('toStub', () => {
 		test('correctly maps a complete record to a stub', () => {
-			const abilityCheckRec: AbilityCheckRec = {
-				AbilityCheckId: abilityCheckId,
-				ActionId: actionId,
-				Skill: skill,
-				DC: dc,
-				SuccessNarrationId: successId,
-				FailureNarrationId: failureId,
-				CriticalSuccessNarrationId: critSuccessId,
-				CriticalFailureNarrationId: critFailureId,
-			};
-
-			const abilityCheckStub: AbilityCheckStub = {
-				id: abilityCheckId,
-				actionId,
-				skill,
-				dc,
-				successNarrationId: successId,
-				failureNarrationId: failureId,
-				criticalSuccessNarrationId: critSuccessId,
-				criticalFailureNarrationId: critFailureId,
-			};
-
 			const testStub = toStub(abilityCheckRec);
 			assert.deepStrictEqual(testStub, abilityCheckStub);
 		});
 
 		test('correctly maps a minimal record to a stub', () => {
-			const abilityCheckRec: AbilityCheckRec = {
-				AbilityCheckId: abilityCheckId,
-				ActionId: actionId,
-				Skill: skill,
-				DC: dc,
-				SuccessNarrationId: null,
-				FailureNarrationId: null,
-				CriticalSuccessNarrationId: null,
-				CriticalFailureNarrationId: null,
-			};
-
-			const abilityCheckStub: AbilityCheckStub = {
-				id: abilityCheckId,
-				actionId,
-				skill,
-				dc,
-				successNarrationId: undefined,
-				failureNarrationId: undefined,
-				criticalSuccessNarrationId: undefined,
-				criticalFailureNarrationId: undefined,
-			};
-
-			const testStub = toStub(abilityCheckRec);
-			assert.deepStrictEqual(testStub, abilityCheckStub);
+			const testStub = toStub(abilityCheckRecMin);
+			assert.deepStrictEqual(testStub, abilityCheckStubMin);
 		});
 	});
 });
---
+++ backend/src/ability-check/fakes/ability-check-data-fake.ts
@@ -0,0 +1,145 @@
+import type {
+	AbilityCheckResponse,
+	AbilityCheckStub,
+} from '#ability-check/ability-check-dtos.ts';
+import type {
+	AbilityCheckRec,
+	AbilityCheckRefRec,
+} from '#ability-check/ability-check-repository.ts';
+import type { NarrationRec } from '#narration/narration-repository.ts';
+import { randomUUID } from 'crypto';
+
+const abilityCheckId = randomUUID();
+const actionId = randomUUID();
+const skill = 'Acrobatics';
+const dc = 15;
+const successId = randomUUID();
+const failureId = randomUUID();
+const critSuccessId = randomUUID();
+const critFailureId = randomUUID();
+
+// @TODO Move to narration fake?
+const success: NarrationRec = {
+	NarrationId: successId,
+	NarrationTemplateId: null,
+	Name: 'Success Narration',
+	Description: 'Success Narration Description',
+	IsRead: true,
+};
+const failure: NarrationRec = {
+	NarrationId: failureId,
+	NarrationTemplateId: null,
+	Name: 'Failure Narration',
+	Description: 'Failure Narration Description',
+	IsRead: false,
+};
+const critSuccess: NarrationRec = {
+	NarrationId: critSuccessId,
+	NarrationTemplateId: null,
+	Name: 'Critical Success Narration',
+	Description: 'Critical Success Narration Description',
+	IsRead: true,
+};
+const critFailure: NarrationRec = {
+	NarrationId: critFailureId,
+	NarrationTemplateId: null,
+	Name: 'Critical Failure Narration',
+	Description: 'Critical Failure Narration Description',
+	IsRead: false,
+};
+
+export const abilityCheckRec: AbilityCheckRec = {
+	AbilityCheckId: abilityCheckId,
+	ActionId: actionId,
+	Skill: skill,
+	DC: dc,
+	SuccessNarrationId: successId,
+	FailureNarrationId: failureId,
+	CriticalSuccessNarrationId: critSuccessId,
+	CriticalFailureNarrationId: critFailureId,
+};
+
+export const abilityCheckRecMin: AbilityCheckRec = {
+	AbilityCheckId: abilityCheckId,
+	ActionId: actionId,
+	Skill: skill,
+	DC: dc,
+	SuccessNarrationId: null,
+	FailureNarrationId: null,
+	CriticalSuccessNarrationId: null,
+	CriticalFailureNarrationId: null,
+};
+
+export const abilityCheckRefRec: AbilityCheckRefRec = {
+	AbilityCheckId: abilityCheckId,
+	ActionId: actionId,
+	Skill: skill,
+	DC: dc,
+	SuccessNarrationId: successId,
+	FailureNarrationId: failureId,
+	CriticalSuccessNarrationId: critSuccessId,
+	CriticalFailureNarrationId: critFailureId,
+	SuccessNarration: success,
+	FailureNarration: failure,
+	CriticalSuccessNarration: critSuccess,
+	CriticalFailureNarration: critFailure,
+};
+
+export const abilityCheckRefRecMin: AbilityCheckRefRec = {
+	AbilityCheckId: abilityCheckId,
+	ActionId: actionId,
+	Skill: skill,
+	DC: dc,
+	SuccessNarrationId: null,
+	FailureNarrationId: null,
+	CriticalSuccessNarrationId: null,
+	CriticalFailureNarrationId: null,
+	SuccessNarration: null,
+	FailureNarration: null,
+	CriticalSuccessNarration: null,
+	CriticalFailureNarration: null,
+};
+
+export const abilityCheckDto: AbilityCheckResponse = {
+	id: abilityCheckId,
+	actionId,
+	skill,
+	dc,
+	successNarration: 'Success Narration Description',
+	failureNarration: 'Failure Narration Description',
+	criticalSuccessNarration: 'Critical Success Narration Description',
+	criticalFailureNarration: 'Critical Failure Narration Description',
+};
+
+export const abilityCheckDtoMin: AbilityCheckResponse = {
+	id: abilityCheckId,
+	actionId,
+	skill,
+	dc,
+	successNarration: undefined,
+	failureNarration: undefined,
+	criticalSuccessNarration: undefined,
+	criticalFailureNarration: undefined,
+};
+
+export const abilityCheckStub: AbilityCheckStub = {
+	id: abilityCheckId,
+	actionId,
+	skill,
+	dc,
+	successNarrationId: successId,
+	failureNarrationId: failureId,
+	criticalSuccessNarrationId: critSuccessId,
+	criticalFailureNarrationId: critFailureId,
+};
+
+export const abilityCheckStubMin: AbilityCheckStub = {
+	id: abilityCheckId,
+	actionId,
+	skill,
+	dc,
+	successNarrationId: undefined,
+	failureNarrationId: undefined,
+	criticalSuccessNarrationId: undefined,
+	criticalFailureNarrationId: undefined,
+};
