9ba99b18536c826378aff9fa03bc2a94df95d013
Dan Malear <danmalear@gmail.com>
2026-05-19 11:34:42 -0700

    Added EventBus test

---
+++ backend/src/framework/event/EventBus.test.ts
@@ -0,0 +1,71 @@
+import { fakeWss } from '#framework/fakes/websocket-fake.ts';
+import { FakeStreamRepository } from '#framework/stream/fakes/FakeStreamRepository.ts';
+import assert from 'assert';
+import { beforeEach, suite, test } from 'node:test';
+import type { Event } from './Event.ts';
+import { EventBus } from './EventBus.ts';
+import { fakeEvent } from './fakes/event-data-fake.ts';
+import { FakeEventRepository } from './fakes/FakeEventRepository.ts';
+import { FakeEventSubscriber } from './fakes/FakeEventSubscriber.ts';
+
+suite('CommandBus', () => {
+	let eventBus: EventBus;
+	const fakeEventHandler = new FakeEventSubscriber();
+	const fakeEventRepository = new FakeEventRepository();
+	const fakeStreamRepository = new FakeStreamRepository();
+
+	beforeEach(() => {
+		fakeEventHandler.resetCalls();
+		eventBus = new EventBus({
+			eventRepository: fakeEventRepository,
+			streamRepository: fakeStreamRepository,
+			wss: fakeWss,
+		});
+	});
+
+	suite('subscribe', () => {
+		test('subscribes a handler to a new context', () => {
+			eventBus.subscribe('Context', fakeEventHandler);
+			assert.strictEqual(eventBus.subscribers['Context'][0], fakeEventHandler);
+		});
+
+		test('subscribes a handler to an existing context', () => {
+			eventBus.subscribers = {
+				Context: [fakeEventHandler],
+			};
+			const newHandler = fakeEventHandler.clone();
+			eventBus.subscribe('Context', newHandler);
+			assert.strictEqual(eventBus.subscribers['Context'][0], fakeEventHandler);
+			assert.strictEqual(eventBus.subscribers['Context'][1], newHandler);
+		});
+
+		test('does not subscribe the same event handler twice', () => {
+			eventBus.subscribers = {
+				Context: [fakeEventHandler],
+			};
+			eventBus.subscribe('Context', fakeEventHandler);
+			assert.strictEqual(eventBus.subscribers['Context'].length, 1);
+			assert.strictEqual(eventBus.subscribers['Context'][0], fakeEventHandler);
+		});
+	});
+
+	suite('send', () => {
+		beforeEach(() => {
+			eventBus.subscribers = {
+				[fakeEvent.context]: [fakeEventHandler],
+			};
+		});
+
+		test('sends a valid event', async () => {
+			await eventBus.send(fakeEvent);
+			assert.strictEqual(fakeEventHandler.calls.handle, 1);
+		});
+
+		test('nothing happens when an invalid context is supplied', async () => {
+			const badEvent = fakeEvent.clone() as Event<string, string, object>;
+			badEvent.context = 'BadContext';
+			await eventBus.send(badEvent);
+			assert.strictEqual(fakeEventHandler.calls.handle, 0);
+		});
+	});
+});
