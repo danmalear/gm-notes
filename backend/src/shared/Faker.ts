f7f0132b4030fc48903fd4635bf63b4cbfa25eab
Dan Malear <danmalear@gmail.com>
2026-05-19 12:23:42 -0700

    (m) Corrected weird wording in comment

--- backend/src/shared/Faker.ts
+++ backend/src/shared/Faker.ts
@@ -1,7 +1,7 @@
 import type { ConditionalKeys } from './utility-types.ts';
 
 export type FakerCalls<T> = {
-	// This usage is fine because I just want to care that the key indexes a function, not how it's called
+	// This usage is fine because I just care that the key indexes a function, not how it's called
 	// eslint-disable-next-line @typescript-eslint/no-unsafe-function-type
 	[K in ConditionalKeys<T, Function>]: number;
 };
