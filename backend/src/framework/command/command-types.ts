727875969e3a9e373ec7715f5c02685e58e80cdb
Dan Malear <danmalear@gmail.com>
2026-05-21 14:53:49 -0700

    Added streamVersion as param to CommandFunction

--- backend/src/framework/command/command-types.ts
+++ backend/src/framework/command/command-types.ts
@@ -1,4 +1,7 @@
 import type { UUID } from 'crypto';
 import type { Command } from './Command.ts';
 
-export type CommandFunction = (command: Command) => Promise<UUID>;
+export type CommandFunction = (
+	command: Command,
+	streamVersion?: number,
+) => Promise<UUID>;
