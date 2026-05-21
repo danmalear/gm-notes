c3590e8bf543674be32b4277198b117fde6f67c2
Dan Malear <danmalear@gmail.com>
2026-05-21 15:00:27 -0700

    Added ApplyEvent command and handler

---
+++ backend/src/framework/event/event-commands.ts
@@ -0,0 +1,127 @@
+import type { CommandFunction } from '#command/command-types.ts';
+import type { Command } from '#command/Command.ts';
+import type { CommandHandlerConfig } from '#command/CommandHandler.ts';
+import { CommandHandler } from '#command/CommandHandler.ts';
+import { RawStream } from '#framework/stream/Stream.ts';
+import { BadRequestError, NotFoundError } from '#shared/error.ts';
+import { isUUID } from '#shared/uuid.ts';
+import type { UUID } from 'crypto';
+import { Event } from './Event.ts';
+
+/**
+ * Applies an event manually, for dev use to make manual changes to the event ledger
+ */
+export interface ApplyEvent {
+	context: string;
+	ref: string;
+	streamId: UUID;
+	streamVersion: number;
+	data: object;
+}
+
+export function validateApplyEvent(
+	command: unknown,
+): asserts command is ApplyEvent {
+	if (!command) {
+		throw new BadRequestError(`No data supplied to Apply Event command`);
+	}
+	if (typeof command !== 'object' || Array.isArray(command)) {
+		throw new BadRequestError(`Invalid data supplied to Apply Event command`);
+	}
+
+	// context: string;
+	if (!('context' in command) || !command.context) {
+		throw new BadRequestError('No context supplied to Apply Event command');
+	}
+	if (typeof command.context !== 'string') {
+		throw new BadRequestError(
+			'Invalid context supplied to Apply Event command',
+		);
+	}
+
+	// ref: string;
+	if (!('ref' in command) || !command.ref) {
+		throw new BadRequestError('No ref supplied to Apply Event command');
+	}
+	if (typeof command.ref !== 'string') {
+		throw new BadRequestError('Invalid ref supplied to Apply Event command');
+	}
+
+	// streamId: UUID;
+	if (!('streamId' in command) || !command.streamId) {
+		throw new BadRequestError('No stream ID supplied to Apply Event command');
+	}
+	if (typeof command.streamId !== 'string' || !isUUID(command.streamId)) {
+		throw new BadRequestError(
+			'Invalid stream ID supplied to Apply Event command',
+		);
+	}
+
+	// streamVersion: number;
+	if (!('streamVersion' in command) || !command.streamVersion) {
+		throw new BadRequestError(
+			'No stream version supplied to Apply Event command',
+		);
+	}
+	if (typeof command.streamVersion !== 'number') {
+		throw new BadRequestError(
+			'Invalid stream version supplied to Apply Event command',
+		);
+	}
+
+	// data: object;
+	if (!('data' in command) || !command.data) {
+		throw new BadRequestError('No data supplied to Apply Event command');
+	}
+	if (typeof command.data !== 'object') {
+		throw new BadRequestError('Invalid data supplied to Apply Event command');
+	}
+}
+
+type EventCommandHandlerConfig = CommandHandlerConfig;
+
+export class EventCommandHandler extends CommandHandler {
+	constructor(config: EventCommandHandlerConfig) {
+		super(config);
+	}
+
+	override async handle(command: Command) {
+		const stream = command.streamId
+			? new RawStream(command.streamId, {
+					eventRepository: this.eventRepository,
+					streamRepository: this.streamRepository,
+				})
+			: undefined;
+		if (command.streamId && !stream) {
+			throw new NotFoundError(`Stream not found with ID ${command.streamId}.`);
+		}
+		await this.validateCommandVersion(stream, command);
+		if (command.context !== 'Event') {
+			throw new BadRequestError(
+				'Non-event command submitted to event command handler',
+			);
+		}
+		switch (command.ref) {
+			case 'Apply':
+				await this.Apply(command, command.streamVersion);
+				break;
+			default:
+				throw new BadRequestError(`Invalid event command: ${command.ref}`);
+		}
+	}
+
+	Apply: CommandFunction = async (command, streamVersion = 0) => {
+		validateApplyEvent(command.data);
+
+		const event = new Event({
+			context: command.data.context,
+			ref: command.data.ref,
+			streamId: command.data.streamId,
+			correlationId: command.correlationId,
+			streamVersion: streamVersion + 1,
+			data: command.data,
+		});
+
+		return await this.eventBus.send(event);
+	};
+}
