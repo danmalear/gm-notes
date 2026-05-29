86971340554306ba38ea1e5cb551c14443556c96
Dan Malear <danmalear@gmail.com>
2026-05-29 10:23:59 -0700

    Added repository interface for prisma use

---
+++ backend/src/shared/repository-interface.ts
@@ -0,0 +1,46 @@
+import type { PrismaClient } from '@prisma/client/extension';
+import type { UUID } from 'crypto';
+
+export interface IRepository<
+	TModel,
+	TCreate,
+	TUpdate,
+	TRefModel extends TModel = TModel,
+> {
+	prisma: PrismaClient;
+
+	/**
+	 * Retrieves a record from the database by its UUID
+	 * @param id UUID of the record to retrieve
+	 * @returns The record with the given UUID, or null if not found
+	 */
+	getByIdRaw(id: UUID): Promise<TModel | null>;
+
+	/**
+	 * Retrieves a record from the database by its UUID and hydrates any foreign key relationships
+	 * @param id UUID of the record to retrieve
+	 * @returns The record with the given UUID, or null if not found
+	 */
+	getById(id: UUID): Promise<TRefModel | null>;
+
+	/**
+	 * Retrieves all records from the database
+	 * @returns An array of all records (empty array if none found)
+	 */
+	getAll(): Promise<TModel[]>;
+
+	/**
+	 * Inserts a new record into the database
+	 * @param data Data to insert into database
+	 * @returns The inserted record
+	 */
+	insert(data: TCreate): Promise<TModel>;
+
+	/**
+	 * Updates an existing database record with new data
+	 * @param id UUID of the existing record to update
+	 * @param data New data to update the record with
+	 * @returns The updated record
+	 */
+	update(id: UUID, data: TUpdate): Promise<TModel>;
+}
