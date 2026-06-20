1125839d78ec8d4c06872aedcc9d1f7336400e57
Dan Malear <danmalear@gmail.com>
2026-06-20 10:38:15 -0700

    (m) imported interface instead of class

--- backend/src/item/item-routes.ts
+++ backend/src/item/item-routes.ts
@@ -2,7 +2,7 @@ import type { ICommandBus } from '#command/command-bus.ts';
 import type { IEventBus } from '#event/event-bus.ts';
 import { getByIdDEPRECATED } from '#shared/route-utils.ts';
 import type { Express } from 'express';
-import type { ItemRepository } from './item-repository.ts';
+import type { IItemRepository } from './item-repository.ts';
 import { toDto } from './location-item-mappers.ts';
 import type { LocationItemRepository } from './location-item-repository.ts';
 
@@ -10,7 +10,7 @@ export function itemRoutes(
 	app: Express,
 	_commandBus: ICommandBus,
 	_eventBus: IEventBus,
-	_itemRepository: ItemRepository,
+	_itemRepository: IItemRepository,
 	locationItemRepository: LocationItemRepository,
 ) {
 	const apiNamespace = 'items';
