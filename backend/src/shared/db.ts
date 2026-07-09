f15cec9b5df9d33c6cf71496734110634deb1e02
Dan Malear <danmalear@gmail.com>
2026-07-09 10:04:10 -0700

    Removed knex from project

--- backend/src/shared/db.ts
+++
@@ -1,8 +0,0 @@
-import knex from 'knex';
-import configs from '../../knexfile.ts';
-
-const env = process.env.NODE_ENV ?? 'development';
-
-const config = configs[env];
-
-export const db = knex(config);
