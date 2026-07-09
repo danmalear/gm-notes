97f441cecd918de5319445232ab51f95377be269
Dan Malear <danmalear@gmail.com>
2026-07-09 12:43:31 -0700

    Added BaseStreamRouter class

--- backend/src/shared/router.ts
+++ backend/src/shared/router.ts
@@ -1,14 +1,22 @@
-import type { Express } from 'express';
+import type { ICommandBus } from '#command/command-bus.ts';
+import type { IEventBus } from '#event/event-bus.ts';
+import type { IEventRepository } from '#event/event-repository.ts';
+import type { IStreamRepository } from '#stream/stream-repository.ts';
+import type { Express, Response } from 'express';
+import type { DataResponse, MessageResponse } from './dtos.ts';
+import { getMessage, NotImplementedError } from './error.ts';
+import type { IRepository } from './repository.ts';
+import { isUUID } from './uuid.ts';
 
 export interface BaseRouterOpts {
 	app: Express;
 }
 
-interface PrivateRouterOpts {
+export interface PrivateBaseRouterOpts {
 	namespace: string;
 }
 
-type AllBaseRouterOpts = BaseRouterOpts & PrivateRouterOpts;
+type AllBaseRouterOpts = BaseRouterOpts & PrivateBaseRouterOpts;
 
 export interface BaseRouterInitOpts<
 	GetOpts,
@@ -94,5 +102,125 @@ export class BaseRouter<
 				message: `DELETE requests not supported for namespace ${this.namespace}`,
 			});
 		});
+	}
+}
+
+export interface BaseStreamRouterOpts<
+	TModel,
+	TCreate,
+	TUpdate,
+	TModelIncludeAll extends TModel,
+> extends BaseRouterOpts {
+	repository: IRepository<TModel, TCreate, TUpdate, TModelIncludeAll>;
+	commandBus: ICommandBus;
+	eventBus: IEventBus;
+	eventRepository: IEventRepository;
+	streamRepository: IStreamRepository;
+}
+
+export interface PrivateBaseStreamRouterOpts extends PrivateBaseRouterOpts {
+	descriptor: string;
+	descriptors: string;
+}
+
+type AllBaseStreamRouterOpts<
+	TModel,
+	TCreate,
+	TUpdate,
+	TModelIncludeAll extends TModel,
+> = BaseStreamRouterOpts<TModel, TCreate, TUpdate, TModelIncludeAll> &
+	PrivateBaseStreamRouterOpts;
+
+/**
+ * Base class for a router that supports an event-sourced stream entity
+ */
+export class BaseStreamRouter<
+	TModel,
+	TCreate,
+	TUpdate,
+	TModelIncludeAll extends TModel,
+	TDto,
+	TStub = TDto,
+	GetOpts = unknown,
+	PostOpts = unknown,
+	PutOpts = unknown,
+	PatchOpts = unknown,
+	DeleteOpts = unknown,
+> extends BaseRouter<GetOpts, PostOpts, PutOpts, PatchOpts, DeleteOpts> {
+	descriptor: string;
+	descriptors: string;
+	commandBus: ICommandBus;
+	eventBus: IEventBus;
+	repository: IRepository<TModel, TCreate, TUpdate, TModelIncludeAll>;
+	eventRepository: IEventRepository;
+	streamRepository: IStreamRepository;
+
+	constructor({
+		app,
+		namespace,
+		descriptor,
+		descriptors,
+		commandBus,
+		eventBus,
+		repository,
+		eventRepository,
+		streamRepository,
+	}: AllBaseStreamRouterOpts<TModel, TCreate, TUpdate, TModelIncludeAll>) {
+		super({
+			app,
+			namespace,
+		});
+		this.descriptor = descriptor;
+		this.descriptors = descriptors;
+		this.commandBus = commandBus;
+		this.eventBus = eventBus;
+		this.repository = repository;
+		this.eventRepository = eventRepository;
+		this.streamRepository = streamRepository;
+	}
+
+	toDto(_model: TModelIncludeAll): TDto {
+		throw new NotImplementedError();
+	}
+
+	toStub(_model: TModel): TStub {
+		throw new NotImplementedError();
+	}
+
+	/**
+	 * Add a default get by ID route
+	 *
+	 * @param opts Option params for get by ID route
+	 */
+	defaultGetById() {
+		this.app.get(
+			`/${this.namespace}/:id`,
+			async (req, res: Response<MessageResponse | DataResponse<TDto>>) => {
+				console.log(
+					`${this.descriptor} GET request received. params: ${JSON.stringify(req.params)}`,
+				);
+
+				if (!isUUID(req.params.id)) {
+					res.status(400).send({ message: 'Invalid UUID format' });
+					return;
+				}
+
+				const model = await this.repository.getById(req.params.id);
+				if (!model) {
+					res.status(404).send({
+						message: `${this.descriptor} with ID ${req.params.id} not found`,
+					});
+					return;
+				}
+
+				try {
+					res.send({ data: this.toDto(model) });
+				} catch (e) {
+					res.status(500).send({
+						message: getMessage(e),
+					});
+				}
+			},
+		);
 	}
 }
